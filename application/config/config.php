f85993f49a0a1be32a346ba7e1d56e75ecf708f3
Netanel Bar Ilan <netanel@americanbars.com>
2016-12-20 12:09:29 -0800

    changed config to fit sandbox again

--- application/config/config.php
+++ application/config/config.php
@@ -23,9 +23,10 @@
 //echo getcwd();die;
 //$config['base_url']    = 'http://sandbox.americanbars.com/';
 
-//$config['base_url'] = ($_SERVER['SERVER_PORT'] == 443 ? 'https' : 'https') . "://{$_SERVER['HTTP_HOST']}/";
-$config['base_url']    = 'http://localhost/webapp';
-$config['base_path']="/Applications/MAMP/htdocs/webapp";
+$config['base_url'] = ($_SERVER['SERVER_PORT'] == 443 ? 'https' : 'https') . "://{$_SERVER['HTTP_HOST']}/";
+//$config['base_url']    = 'http://localhost/webapp';
+//$config['base_path']="/Applications/MAMP/htdocs/webapp";
+$config['base_path']="/var/www/html/"
 /*
  * RewriteEngine on
 RewriteRule ^check_rewrite.php$ mod_rewrite.php
