a259a76e85f3578d490f94765ecdf6b38f85735b
Netanel Bar Ilan <netanel@americanbars.com>
2017-01-01 17:30:35 -0800

    set config path to be a path on server

--- system/core/Common.php
+++ system/core/Common.php
@@ -223,7 +223,7 @@ if ( ! function_exists('get_config'))
 		}
 
 		// Is the config file in the environment folder?
-		if ( ! defined('ENVIRONMENT') OR ! file_exists($file_path = APPPATH.'config/'.ENVIRONMENT.'/config.php'))
+		if (!file_exists($file_path = '/var/config/ab/application/config.php'))
 		{
 			$file_path = APPPATH.'config/config.php';
 		}
