88a9b7b2bccb9c0f535dc0ff1c218525d685c64f
Netanel Bar Ilan <netanel@americanbars.com>
2017-01-03 19:11:24 -0800

    don't remove ' from search string

--- application/admin/controllers/bar.php
+++ application/admin/controllers/bar.php
@@ -163,7 +163,6 @@ class Bar extends  CI_Controller {
 		}
 		
 		$keyword=str_replace('"','',str_replace(array(",","%","$","&","*","#","(",")",":",";",">","<","/"),'',trim($keyword)));
-                $keyword=str_replace("'", "\\'", trim($keyword));
 	
 		$config['uri_segment']='7';
 		$config['base_url'] = base_url().'bar/search_list_bar'.'/'.$bar_type."/".$limit.'/'.$option.'/'.$keyword.'/';
