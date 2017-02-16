6720ec1502072a95d1d2a99a1b74115bbf77bdd5
Netanel Bar Ilan <netanel@americanbars.com>
2017-02-15 16:42:19 -0800

    added facebook and twitter buttons

--- application/views/default/home/socialshare1.php
+++ application/views/default/home/socialshare1.php
@@ -280,7 +280,7 @@ foreach ($providers as $provider => $d) {
                     </div>                
                     <div class="dashboard_subblock">              
                         <div class="padtb" style="text-align: center;">
-                            <div class="input_box col-sm-10 text-center">                               
+                            <div class="input_box text-center">                               
                                 <textarea type="text" class="form-control form-pad" id="message" name="message" style="width: 400px ; height: 100px;display: block; margin-left: auto; margin-right: auto;" placeholder="What's on your mind?"></textarea>
                             </div>
                             <div class="clearfix"></div>
