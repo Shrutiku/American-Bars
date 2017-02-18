bfa94dd90bcfebc6edca4bafd7a2bb44704f0cd5
Netanel Bar Ilan <netanel@americanbars.com>
2017-02-17 20:20:02 -0800

    fixed socialshare

--- application/views/default/home/socialshare1.php
+++ application/views/default/home/socialshare1.php
@@ -176,8 +176,6 @@ $config = array(
     });*/
 </script>
 
-<!-- doT.js for rendering templates -->
-<script src="<?php echo app_bower_url(); ?>/doT/doT.min.js"></script>
 <!-- Codebird.js - required for TWITTER -->
 <script src="<?php echo app_bower_url(); ?>/codebird-js/codebird.js"></script>
 <!-- Moment.js for showing "time ago" and/or "date"-->
@@ -185,7 +183,9 @@ $config = array(
 <!-- Moment Locale to format the date to your language (eg. italian lang)-->
 <script src="<?php echo app_bower_url(); ?>/moment/locale/en.js"></script>
 <!-- Social-feed js -->
-<script src="<?php echo app_bower_url(); ?>/social-feed/js/jquery.socialfeed.js"></script>-->
+<script src="<?php echo app_bower_url(); ?>/social-feed/js/jquery.socialfeed.js"></script>
+<!-- doT.js for rendering templates -->
+<script src="<?php echo app_bower_url(); ?>/doT/doT.min.js"></script>
 
 <script>
     $(document).ready(function () {
