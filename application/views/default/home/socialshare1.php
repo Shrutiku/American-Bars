6169a4ed3053bc1c99703eb017b377266b70c5bb
Netanel Bar Ilan <netanel@americanbars.com>
2017-02-18 13:42:25 -0800

    fixed socialshare

--- application/views/default/home/socialshare1.php
+++ application/views/default/home/socialshare1.php
@@ -257,13 +257,9 @@ foreach ($providers as $provider => $d) {
     });
     
     $("#picture").change(function(){
-    var img = $('#picture')[0].files[0];
-        console.log(img + "\n\n\n");
-        console.log($('#picture')[0].files[0]);
-        if (img == '')
-        {
-            return;
-        }
+    var img = $('#picture')[0].files[0].getAsBinary();
+        //console.log(img + "\n\n\n");
+        //console.log($('#picture')[0].files[0]);
         
         $.ajax({
           url: 'https://api.imgur.com/3/image',
