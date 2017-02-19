bc3512904605740674fbeeffd7cb10c54a8dd73a
Netanel Bar Ilan <netanel@americanbars.com>
2017-02-18 18:36:18 -0800

    fixed socialshare

--- application/views/default/home/socialshare1.php
+++ application/views/default/home/socialshare1.php
@@ -317,7 +317,7 @@ foreach ($providers as $provider => $d) {
           data: formData,
           success: function(response) {
             console.log(response);
-            document.getElementById("picture").value = response.data.link;
+            document.getElementById("picture").value = "\"" + response.data.link + "\"";
           },
           cache: false,
           contentType: false,
