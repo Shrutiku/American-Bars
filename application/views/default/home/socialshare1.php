663858920a6b560cfc2400215cd2246aecfc1897
Netanel Bar Ilan <netanel@americanbars.com>
2017-02-23 16:30:07 -0800

    added choice of page

--- application/views/default/home/socialshare1.php
+++ application/views/default/home/socialshare1.php
@@ -129,7 +129,7 @@ foreach ($providers as $provider => $d) {
     if ($d && !empty($d['user_profile'])) {
         if (!empty($d['user_profile']->accounts)) {
             ?> 
-            choosepage("<?php echo $provider;?>",<?php echo json_encode($d['user_profile']->accounts);?>);
+            choosepage("<?php echo $provider;?>",JSON.parse(<?php echo json_encode($d['user_profile']->accounts);?>));
          <?php
         }
     }
