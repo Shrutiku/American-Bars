fab4b8fe62a270a1f590514185d8e752f63c4ae8
Netanel Bar Ilan <netanel@americanbars.com>
2017-02-27 13:34:28 -0800

    added claimbar landing page

---
+++ application/views/default/home/claimbar.php
@@ -0,0 +1,25 @@
+<script type="text/javascript" src="<?php echo base_url().getThemeName(); ?>/assets/plugins/jquery-validation/dist/jquery.validate.js"></script>
+
+<!-- ########################################################################################### -->
+<!-- content -->
+<div class="wrapper row6 padtb10">
+     	<div class="container">
+     		<div class="result_box clearfix mar_top30bot20">
+     			<div class="login_block br_green_yellow" style="width: 90%;">
+     				<div class="result_search">
+	     				<div class="result_search_text">Claim Your Bar in 3 Easy Steps</div>
+     				</div>
+				
+                            
+                     
+     				<div class="pad20">
+     				<h1 class="yellow_title padb10 br_bott_gray">Start Here With Step 1.  </h1>
+					<form class=""  role="form" action="<?php echo site_url("bar/lists") ?>" method="post">
+                			<div class="pull-left"><input type="text" name="bar_title_new" id="bar_title_new" class="form-control bar_title_new" placeholder="By Name, City Or Zip"></div>
+                    			<div class="pull-left"><button class="btn btn-lg btn-primary btn-block yellowlink   " type="submit">Claim Your Bar Now!</button></div>
+              </form>
+     			</div>
+     		</div>
+   		</div>
+   	</div>
+<!-- ************************************************************************ -->
