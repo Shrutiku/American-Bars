02828e771f552c1a152e6c13b38f3583ee82fdbc
Netanel Bar Ilan <netanel@americanbars.com>
2017-02-27 16:05:36 -0800

    added claimbar landing

--- application/views/default/home/claimbar.php
+++ application/views/default/home/claimbar.php
@@ -15,15 +15,15 @@
                                                                                 <p style="text-align: center">Search Your Bar Profile</p>
                                                                                 <h1 class="yellow_title padb10 br_bott_gray" style="text-align: center;">Step 2:</h1>
                                                                                 <p style="text-align: center;">Click on 'Claim This Bar' Button</p>
-                                                                                <img src="<?php echo base_url() . getThemeName(); ?>/images/claimbar_search_result_ex.png" border="0" style="max-width:100%;max-height:60%;">
+                                                                                <img src="<?php echo base_url() . getThemeName(); ?>/images/claimbar_search_result_ex.png" border="0" style="max-width:60%;max-height:60%;">
                                                                                 <h1 class="yellow_title padb10 br_bott_gray" style="text-align: center;">Step 3:</h1>
                                                                                 <p style="text-align: center;">Finish the Easy and Quick Claim Process</p>
 
      				<div class="pad20">
      				<h1 class="yellow_title padb10 br_bott_gray" style="text-align: center;">Start Here With Step 1 </h1>
 					<form class=""  role="form" style="text-align: center;" action="<?php echo site_url("bar/lists") ?>" method="post">
-                			<div class="pull-left" style="text-align: center;"><input type="text" name="bar_title_new" id="bar_title_new" class="form-control bar_title_new" placeholder="By Name, City Or Zip"></div>
-                    			<div class="pull-left" style="text-align: center;"><button class="btn btn-lg btn-primary btn-block yellowlink   " type="submit">Claim Your Bar Now!</button></div>
+                			<div class="pull-left" style="text-align: center;"><input type="text" name="bar_title_new" style="text-align: center;" id="bar_title_new" class="form-control bar_title_new" placeholder="By Name, City Or Zip"></div>
+                    			<div class="pull-left" style="text-align: center;"><button class="btn btn-lg btn-primary btn-block yellowlink  style="text-align: center;" " type="submit">Claim Your Bar Now!</button></div>
               </form>
      			</div>
      		</div>
