9178312a638500a49762818fd34eed722164dcc5
Netanel Bar Ilan <netanel@americanbars.com>
2017-03-06 11:05:13 -0800

    added metrics

--- application/controllers/metrics.php
+++ application/controllers/metrics.php
@@ -24,7 +24,7 @@ class Metrics extends REST_Controller {
                 $this->load->model('bar_model');
                 $this->load->model('user_model');
 
-                $data['hulfmug_bars'] = $this->bar_model->get_total_bar_count_by_type("half_mug");
+                $data['hulfmug_bars'] = $this->bar_model->get_total_bar_count_by_type("half_mug_claimed_bar");
                 $data['fullmug_bars'] = $this->bar_model->get_total_bar_count_by_type("full_mug");
                 $data['enthusiasts'] = $this->user_model->get_total_user_count("user", "active");
 		
