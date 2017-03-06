c0d11a82910e55e81f4d236b5b888eee65f679fb
Netanel Bar Ilan <netanel@americanbars.com>
2017-03-06 10:46:20 -0800

    added metrics

--- application/controllers/metrics.php
+++ application/controllers/metrics.php
@@ -29,10 +29,8 @@ class Metrics extends SPACULLUS_Controller {
                 $data['hulfmug_bars'] = $this->bar_model->get_total_bar_count_by_type("half_mug");
                 $data['fullmug_bars'] = $this->bar_model->get_total_bar_count_by_type("full_mug");
                 $data['enthusiasts'] = $this->user_model->get_total_user_count("user", "active");
-                $theme = getThemeName ();
-
 		
-                $this->load->view($theme .'/home/metrics', $data);
+                $this->response($data ,200);
 	}
 }
 ?>
