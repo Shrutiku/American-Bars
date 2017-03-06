1d891bbc8237d7dcee59faef2ca61d46f22b32d3
Netanel Bar Ilan <netanel@americanbars.com>
2017-03-06 13:58:42 -0800

    made the claim bar button green

--- application/views/default/bar/halfmug_bar_detail.php
+++ application/views/default/bar/halfmug_bar_detail.php
@@ -291,13 +291,13 @@ function piShare()
 								<!-- <a id="total-fav" href="javascript:void(0);" name="2" class="btn btn-lg btn-primary full-btn mart10">Add To Favoritess</a> -->
 								
 									<?php
+                                                                        		if($bar_detail['claim']=='unclaimed'  && get_authenticateUserID()==''){?>
+                                    <a href="<?php echo site_url('home/claim_bar_owner_register/'.base64_encode('1V1').'/1V1/'.base64_encode($bar_detail['bar_id']));?>" style="background-color: #4CAF50;" class="review text-center"><b>Claim This Bar</b></a>
+						        	<?php }
 						    		 if($this->session->userdata('user_type')!='bar_owner')
 		{
 						    		 $cnt_fav = fav_checker_bar($bar_detail['bar_id'],$this->session->userdata('user_id')); 
-			
-												 if($bar_detail['claim']=='unclaimed'  && get_authenticateUserID()==''){?>
-                                    <a href="<?php echo site_url('home/claim_bar_owner_register/'.base64_encode('1V1').'/1V1/'.base64_encode($bar_detail['bar_id']));?>" style="background-color: #4CAF50;" class="review text-center"><b>Claim This Bar</b></a>
-						        	<?php }
+	
 											if($cnt_fav==2 && get_authenticateUserID()!=''){
 											?>
 											<a id="total-fav" href="javascript:void(0);" name="2" class="btn btn-lg btn-primary full-btn mart10">Add to My Bar List</a>
