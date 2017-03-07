26917fbd4b314023018e4f119500fdebb596678d
Netanel Bar Ilan <netanel@americanbars.com>
2017-03-07 13:14:00 -0800

    disabled pushnotiifcations

--- application/bower_components/jquery-fireworks/jquery.fireworks.js
+++ application/bower_components/jquery-fireworks/jquery.fireworks.js
@@ -270,7 +270,7 @@
     launch_int = setInterval(launch, 800);
     loop_int = setInterval(loop, 1000 / 50);
     
-    setTimeout(function () {$('canvas').remove(); $('audio').pause(); $('audio').currentTime = 0; $('audio').remove();}, 8000);
+    setTimeout(function () {$('canvas').remove(); .each(function(){ this.pause(); this.currentTime = 0; }); $('audio').remove();}, 8000);
 
     return fireworksField;
   };
