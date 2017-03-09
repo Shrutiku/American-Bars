cb3fdf07e3e8fbcdb1802864379a33c909daf90c
Brian <brian@americanbars.com>
2017-03-09 13:51:26 -0800

    added spacing in claimbar page

--- application/views/default/home/claimbar.php
+++ application/views/default/home/claimbar.php
@@ -14,11 +14,11 @@
                                                                                 <h1 class="yellow_title padb10 br_bott_gray" style="text-align: center;">Step 2: Click on 'Claim This Bar' Button</h1>
                                                                                 <br>
                                                                                 <img src="<?php echo base_url() . getThemeName(); ?>/images/claimbar_search_result_ex.png" border="0" style="max-width:100%;max-height:20%;">
-                                                                                <h1 class="yellow_title padb10 br_bott_gray" style="text-align: center;">Step 3:</h1>
-                                                                                <p style="text-align: center;">Finish the Easy and Quick Claim Process</p>
+                                                                                <h1 class="yellow_title padb10 br_bott_gray" style="text-align: center;">Step 3: Finish the Easy and Quick Claim Process</h1><br/>
+                                                                                
 
      				<div class="pad20">
-     				<h1 class="yellow_title padb10 br_bott_gray" style="text-align: center;">Start Here With Step 1 </h1>
+     				<h1 class="yellow_title padb10 br_bott_gray" style="text-align: center;">Start Here With Step 1: </h1>
 					<form class=""  role="form" action="<?php echo site_url("bar/lists") ?>" method="post">
                 			<div class=""><input type="text" name="bar_title_new"  id="bar_title_new" class="form-control bar_title_new" placeholder="By Name, City Or Zip"></div>
                     			<div class=""><button class="btn btn-lg btn-primary btn-block yellowlink" type="submit">Claim Your Bar Now!</button></div>
