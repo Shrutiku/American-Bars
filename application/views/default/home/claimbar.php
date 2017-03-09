264e303835869aba8951c05382bd5cc85dc0bed8
Brian <brian@americanbars.com>
2017-03-09 14:02:42 -0800

    changed font

--- application/views/default/home/claimbar.php
+++ application/views/default/home/claimbar.php
@@ -6,21 +6,21 @@
      	<div class="container">
      		<div class="result_box clearfix mar_top30bot20">
      			<div class="login_block br_green_yellow" style="width: 90%;">
-     				<div class="result_search">
+     				<div class="result_search"><br/>
 	     				<div class="result_search_text" style="text-align: center">Claim Your Bar in 3 Easy Steps</div>
      				</div>                            
                                                       				<h1 class="yellow_title padb10 br_bott_gray" style="text-align: center;">Step 1: Search Your Bar Profile</h1>
                                                                                 <br>
                                                                                 <h1 class="yellow_title padb10 br_bott_gray" style="text-align: center;">Step 2: Click on 'Claim This Bar' Button</h1>
                                                                                 <br>
-                                                                                <img src="<?php echo base_url() . getThemeName(); ?>/images/claimbar_search_result_ex.png" border="0" style="max-width:100%;max-height:20%;">
+                                                                                <img src="<?php echo base_url() . getThemeName(); ?>/images/claimbar_search_result_ex.png" border="0" style="max-width:100%;max-height:20%;"><br/><br/>
                                                                                 <h1 class="yellow_title padb10 br_bott_gray" style="text-align: center;">Step 3: Finish the Easy and Quick Claim Process</h1><br/>
                                                                                 
 
      				<div class="pad20">
      				<h1 class="yellow_title padb10 br_bott_gray" style="text-align: center;">Start Here With Step 1: </h1>
 					<form class=""  role="form" action="<?php echo site_url("bar/lists") ?>" method="post">
-                			<div class=""><input type="text" name="bar_title_new"  id="bar_title_new" class="form-control bar_title_new" placeholder="By Name, City Or Zip"></div>
+                			<div class=""><input type="text" name="bar_title_new"  id="bar_title_new" class="form-control bar_title_new" placeholder="Search by Bar Name, City or Zip"></div>
                     			<div class=""><button class="btn btn-lg btn-primary btn-block yellowlink" type="submit">Claim Your Bar Now!</button></div>
               </form>
      			</div>
