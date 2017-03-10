aaff760d99a9ac148e854a0c7f322d8386885bee
Brian <brian@americanbars.com>
2017-03-10 08:56:59 -0800

    [adjusted spacing on claim bar page]

--- application/views/default/home/claimbar.php
+++ application/views/default/home/claimbar.php
@@ -14,13 +14,13 @@
                                                                                 <h1 class="yellow_title padb10 br_bott_gray" style="text-align: center;">Step 2: Click on 'Claim This Bar' Button</h1>
                                                                                 <br>
                                                                                 <img src="<?php echo base_url() . getThemeName(); ?>/images/claimbar_search_result_ex.png" border="0" style="max-width:100%;max-height:20%;"><br/><br/>
-                                                                                <h1 class="yellow_title padb10 br_bott_gray" style="text-align: center;">Step 3: Finish the Easy and Quick Claim Process</h1><br/>
+                                                                                <br><h1 class="yellow_title padb10 br_bott_gray" style="text-align: center;">Step 3: Finish the Easy and Quick Claim Process</h1><br/>
                                                                                 
 
      				<div class="pad20">
      				<h1 class="yellow_title padb10 br_bott_gray" style="text-align: center;">Start Here With Step 1: </h1>
 					<form class=""  role="form" action="<?php echo site_url("bar/lists") ?>" method="post">
-                			<div class=""><input type="text" name="bar_title_new"  id="bar_title_new" class="form-control bar_title_new" placeholder="Search by Bar Name, City or Zip"></div>
+                			<div class=""><input type="text" name="bar_title_new"  id="bar_title_new" class="form-control bar_title_new" style="text-align: center" placeholder="Search by Bar Name, City or Zip"></div>
                     			<div class=""><button class="btn btn-lg btn-primary btn-block yellowlink" type="submit">Claim Your Bar Now!</button></div>
               </form>
      			</div>
