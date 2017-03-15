ed0fd7071e92a1ee0a4243edcbd116121e6881f7
Netanel Bar Ilan <netanel@americanbars.com>
2017-03-15 14:34:39 -0700

    fixed google share

--- application/third_party/hybridauth/Hybrid/Providers/Google.php
+++ application/third_party/hybridauth/Hybrid/Providers/Google.php
@@ -319,7 +319,7 @@ class Hybrid_Providers_Google extends Hybrid_Provider_Model_OAuth2 {
                 $params['object.attachments[].url'] = $status['picture'];
             }*/
 
-            $response = $this->api->post("https://www.googleapis.com/plusDomains/v1/people/$user_id/activities", json_encode($params));
+            $response = $this->api->post("https://www.googleapis.com/plusDomains/v1/people/$user_id/activities", $params);
             throw new Exception(json_encode($response));
         }   
 }
