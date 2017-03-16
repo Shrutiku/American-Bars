7b22afa7ac1a56489ed1ec70c40baee006ecfc1c
Anthony <anthony@americanbars.com>
2017-03-16 14:15:00 -0700

    reverted postcard

--- application/admin/views/default/layout/postcard/printpostcard.php
+++ application/admin/views/default/layout/postcard/printpostcard.php
@@ -27,38 +27,31 @@ $html = '';
             // </td>';
 // <img src="'.front_base_url().'default/images/post.png" style="float:left;" />$html .= '</tr></table>';
 
-$html .= '<div style="width:6in; height:4in; font-family: Calibri; border: solid 3px #CCCCCC; color: #000; overflow: hidden;  font-size: 11px;letter-spacing: .8px;">
-
-  <div  style="width:3in; float: left;">
-    <div style="float: left; padding-left: 6.25%; padding-top: 31.25%">
-
-      <h1 style="font-family: Calibri;  font-size: 18px; letter-spacing: .8px;">Dear Bar Owner,</h1>
-
-       <p style="font-family: Calibri; font-size: 16px; font-weight: normal; margin-bottom: 0;letter-spacing: .8px;text-align: justify; height: 31.25%; width: 88%;" >'.substr(nl2br($one_order['post_message']),0,240).'--><!--Max 240 Characters--></p>
-
-         <label style="font-family: Calibri; font-size: 13px; font-weight: bold; float:left; padding-top: 20%; align-self: center;">To read all messages, <br>claim your bar and use code: '.$code.'</label>
-      </div>
-    </div>
-  <div style="width:3in; float: right;">
-
-    <div style="text-align: center; border: solid 1px #996E13; width: .75in; height: .75in; float: right; font-size: 16px;"><br>Stamp</div>
-    <div style="float: right; padding-right: 6.25%; padding-top: 37.5%">
-    
-      <div style="font-family: Calibri; text-align: center; font-size: 11px;">
-                      '.$one_order['bar_title'].' <br/>
-                      '.$one_order['address'].' <br/>
-                      '.$one_order['city']." ".$one_order['state']." ".$one_order['zipcode'].'        
-      </div>
-
-    </div>
-  </div>
-</div>';
+$html .= '<div style="width:600px; height:300px; font-family: Calibri; margin:130px auto; border: solid 3px #CCCCCC; color: #000; overflow:hidden;  font-size: 11px;letter-spacing: .8px;">';
+$html .= '<table><tr><td  style="width:300px">';
+$html .='<div style="float: left;padding: 10px; border-right: solid 1px #996E13;padding-top: 0px;">
+  			<h1 style="font-family: Calibri;  font-size: 11px; letter-spacing: .8px;">Dear Bar Owner,</h1>
+  			<div style="font-family: Calibri; font-size: 11px;font-weight: normal;margin-bottom: 0;letter-spacing: .8px;text-align: justify;height: 215px; " >'.substr(nl2br($one_order['post_message']),0,1000).'</div>
+  			<label style="font-family: Calibri; margin-top:25px; font-size: 11px; 	font-weight: bold; float:left;">'.$one_order['post_title'].'</label>
+  			</div></td><td style="width:300px">
+  			<div style="float: left;padding: 10px; ">
+  			
+  			<div style="border: solid 1px #996E13;width: 95px;height: 100px;float: right; margin-left:150px; margin-top:-50px; padding: 10px;font-size: 16px;">Place Stamp Here</div>
+  			<div style="clear:both;"></div><br>
+  			<div style="font-family: Calibri; text-align: center; margin-top:20px; font-size: 11px;">
+	     							'.$one_order['bar_title'].' <br/>
+	     							'.$one_order['address'].' <br/>
+	     							'.$one_order['city']." ".$one_order['state']." ".$one_order['zipcode'].' 	     	
+  		</div>
+                <label style="font-family: Calibri; margin-top:25px; font-size: 11px; 	font-weight: bold; float:left;">Enter your code: '.$code.'</label>
+  		</div></td></tr></table>';
+$html .= '</div>';
 // 
  // print_r($html);
  // die;
 $dompdf = new DOMPDF();
 $dompdf->load_html($html);
-// $dompdf->set_paper('letter', 'portrait');
+$dompdf->set_paper('letter', 'portrait');
 $dompdf->render();
 // 
 // print_r($dompdf->render());
