2714c1d361f9366221785e699c28472eb5c6e036
Anthony <anthony@americanbars.com>
2017-03-21 10:42:28 -0700

    added name to address field plus font edits

--- application/admin/views/default/layout/postcard/printpostcard.php
+++ application/admin/views/default/layout/postcard/printpostcard.php
@@ -38,9 +38,10 @@ $html = '';
 $html .= '<div style="width:4in; height:6in; font-family: courier; color: #000; overflow: hidden; font-size: 11px; letter-spacing: .8px; <!--border: solid 1px #996E13;--> <!--rotate: 90;--> <!--transform: rotate(90deg) scale(1.03);-->">';
 $html .= '<div style="width: 0in; height:100%;rotate:90; <!--border: solid 1px #996E13;-->"></div>
         <div style="width:2in;rotate:90; height:100%;">
-            <div style="<!--border: solid 1px #996E13;--> width: 100%; height:2in;"></div>
+            <div style="<!--border: solid 1px #996E13;--> width: 100%; height:1in;"></div>
             <div style="text-align: left;">
-              <div style="font-family: courier; text-align: center; font-size: 11px; <!--border: solid 1px #996E13;-->"><p>
+              <div style="font-family: courier; text-align: left; font-size: 16px; <!--border: solid 1px #996E13;-->"><p>
+                              '.$one_order['bar_first_name']." ".$one_order['bar_last_name'].' <br/>
                               '.$one_order['bar_title'].' <br/>
                               '.$one_order['address'].' <br/>
                               '.$one_order['city'].", ".$one_order['state']." ".$one_order['zipcode'].'</p>
@@ -51,9 +52,9 @@ $html .= '<div style="width: 0in; height:100%;rotate:90; <!--border: solid 1px #
             <div style="text-align: center; <!--border: solid 1px #996E13;--> width: 100%; height: .75in; font-size: 16px;"></div>
             <div style="">
 
-              <p style="font-family: courier; font-weight: bold; font-size: 16px; letter-spacing: .8px;">Dear Bar Owner,</p>
+              <p style="font-family: courier; font-weight: bold; font-size: 20px; letter-spacing: .8px;">Dear Bar Owner,</p>
 
-               <p style="font-family: courier; font-size: 14px; letter-spacing: .8px;text-align: left;" >'.substr(nl2br($one_order['post_message']),0,150).'.....<br></p>
+               <p style="font-family: courier; font-size: 16px; letter-spacing: .8px;text-align: left;" >'.substr(nl2br($one_order['post_message']),0,100).'.....<br></p>
 
                  <p style="font-family: courier; font-size: 16px; font-weight: bold; text-align: right; color: #4CAF50;"><br>To read the rest of this message, claim your bar with this code:<br>'.$code.'</p>
               </div>
