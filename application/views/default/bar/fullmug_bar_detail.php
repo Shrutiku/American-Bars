06de173673fbd7f692986cfd1e04eeebc023d5e3
Netanel Bar Ilan <netanel@americanbars.com>
2017-03-22 15:13:46 -0700

    added followers to bar profile

--- application/views/default/bar/fullmug_bar_detail.php
+++ application/views/default/bar/fullmug_bar_detail.php
@@ -47,6 +47,7 @@
      		<div class="result_search">
      			<div class="pull-left">
 	     			<div class="result_search_text"><?php echo "Welcome to " .$bar_detail['bar_title'];?></div>
+                                <div class="result_search_text"><?php echo ($bar_detail['followers'] + count($bar_liker))." Followers";?>	</div>
 	     			
 	            </div>
 	            
