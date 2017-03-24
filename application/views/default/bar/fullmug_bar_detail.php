2d6d2872dfd72e13cfa8350c1148a0c633f51411
Netanel Bar Ilan <netanel@americanbars.com>
2017-03-24 14:04:48 -0700

    added foolowers icon

--- application/views/default/bar/fullmug_bar_detail.php
+++ application/views/default/bar/fullmug_bar_detail.php
@@ -46,9 +46,9 @@
      	<div class="container">
      		<div class="result_search">
      			<div class="pull-left">
-                            <div class="result_search_text" style="white-space:nowrap;">
+                            <div class="result_search_text">
                                 <?php echo "Welcome to " .$bar_detail['bar_title'];?><b style="padding-left:1em;"></b>
-                                 <img src ="<?php echo base_url().'default';?>/images/Team_icon_-_noun_project_20586.svg.png" style="width: 2%;height: 2%;display: inline-block;white-space:nowrap;float: left;">
+                                <div style="white-space:nowrap;"><img src ="<?php echo base_url().'default';?>/images/Team_icon_-_noun_project_20586.svg.png" style="width: 2%;height: 2%;display: inline-block;white-space:nowrap;float: left;"></div>
                                 <b style="color:black;font-weight: normal;font-size:18px;"><?php echo ($bar_detail['followers'] + count($bar_liker))." Followers";?></b>
                             </div>	     			
 	            </div>
