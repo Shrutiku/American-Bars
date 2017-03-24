d867a83f3412af9d9c593e150cb9b1c95cee17d8
Netanel Bar Ilan <netanel@americanbars.com>
2017-03-23 17:02:30 -0700

    added foolowers icon

--- application/views/default/bar/halfmug_bar_detail.php
+++ application/views/default/bar/halfmug_bar_detail.php
@@ -250,9 +250,9 @@ function piShare()
       	<div class="full_mug">
      		<div class="result_search">
      			<div class="pull-left">
-                            <div class="result_search_text" style="font-size: 30px;"><?php echo "Welcome to " .$bar_detail['bar_title'];?>
+                            <div class="result_search_text"><?php echo "Welcome to " .$bar_detail['bar_title'];?>
                                         <b style="padding-left:1em;"></b>
-                                        <img src ="<?php echo base_url().'default';?>/images/Team_icon_-_noun_project_20586.svg.png" style="width: 2%;height: 2%;">
+                                        <img src ="<?php echo base_url().'default';?>/images/Team_icon_-_noun_project_20586.svg.png" style="width: 2%;height: 2%;display: block;">
                                 <b style="color:black;font-weight: normal;font-size:18px;"><?php echo ($bar_detail['followers'] + count($bar_liker))." Followers";?></b>
                             </div>
 	            </div>
