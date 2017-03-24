e5c3b947c2a78a4d0cad6f3b9de8d4e1c233bcee
Brian <brian@americanbars.com>
2017-03-24 16:09:23 -0700

    [amb edit 5]

--- application/views/default/pages/ambassador.php
+++ application/views/default/pages/ambassador.php
@@ -1,23 +1,23 @@
 <div style="padding: 20px">
     <div class="container">
 <h1 class="productbar_title mar_top20 padding-10"><center>Become an American Bars Ambassador</center></h1><br/>
-<h3><center>Start Making Money Today</center></h3><br/>
+<h3><center>Start Making Money Today!</center></h3><br/>
 <p>You can earn hundreds or thousands of dollars a month.  Really, you can!</p>
 <p>Let me show you how.</p>
 <p>For every bar you recruit as a full mug, paying member to American Bars, you get paid $50.  Then, you get paid $25 a month, for the life of the account if you do the following:</p>
 <p>Contact the bar and make sure they update their events, specials, and beverage list and photo gallery.  All of this can be done in less than 15 min.  That is an hourly rate of $100 per hour on average.</p>
-<p><center><b>If you recruit 10 bars, you can make $250 a month.<br/>
+<h2><center><b>If you recruit 10 bars, you can make $250 a month.<br/>
 20 Bars makes you $500 a month.<br/>
 50 Bars makes you $1250 a month.<br/>
 And, if you sign up 100 bars, $2500 a month!<br/>
-</center></b></p>
+</center></b></h2>
 <p>How do you get started?  It is easy:</p>
 <ol type="1">
-    <li>1.  Fill out our application online.</li>
-    <li>2.  Have a brief interview call with an authorized agent.</li>
-    <li>3.  Get you approval code.</li>
-    <li>4.  Start signing up bars from you phone or laptop.</li>
-    <li>5.  Get paid and perform monthly maintenance.</li>
+    <li>Fill out our application online.</li>
+    <li>Have a brief interview call with an authorized agent.</li>
+    <li>Get you approval code.</li>
+    <li>Start signing up bars from you phone or laptop.</li>
+    <li>Get paid and perform monthly maintenance.</li>
 </ol>
 <p>It’s that easy!  Interested?  Click here!</p>
 
