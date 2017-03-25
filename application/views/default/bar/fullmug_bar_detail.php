acbc62c736e38bf85450d093ec2de819ef5b4921
Netanel Bar Ilan <netanel@americanbars.com>
2017-03-24 17:20:20 -0700

    fixed followers icon

--- application/views/default/bar/fullmug_bar_detail.php
+++ application/views/default/bar/fullmug_bar_detail.php
@@ -49,7 +49,7 @@
                             <div class="result_search_text">
                                 <?php echo "Welcome to " .$bar_detail['bar_title'];?>
                                 <b style="padding-left:1em;"></b>
-                                <img src ="<?php echo base_url().'default';?>/images/Team_icon_-_noun_project_20586.svg.png" style="width: 1.5%;height: 1.5%;white-space: nowrap;overflow: hidden;display: block;text-overflow: ellipsis;">
+                                <img src ="<?php echo base_url().'default';?>/images/Team_icon_-_noun_project_20586.svg.png" style="width: 1.5%;height: 1.5%;overflow: hidden;display: block;text-overflow: ellipsis;">
                                 <b style="color:black;font-weight: normal;font-size:18px;"><?php echo ($bar_detail['followers'] + count($bar_liker))." Followers";?></b>
                             </div>	     			
 	            </div>
