6c52e5a54c01ff43f3a858dde2fc62f0b71eaab7
Shrutiku <shrutikulkarni1311@gmail.com>
2017-03-28 00:06:16 -0700

    css

--- application/views/default/pages/careers.php
+++ application/views/default/pages/careers.php
@@ -4,10 +4,10 @@
     .productbar_title mar_top20 padding-10{
         background-image:url('C:\Users\SHRUTI\Documents\images.jpg');
     }
-    #i2{ background:#000000; padding: 0px 0px 100px 100px;}
+    #i2{ background:#000000; padding: 0px 0px 100px 0px;}
 </style>
 <div id="i2" >
-    <img src="http://www.snnbuilders.com/images/meet_us/career.jpg" style="width:1400px;height:300px;">
+    <img src="http://www.snnbuilders.com/images/meet_us/career.jpg" style="width:1000px;height:300px;">
     <br><br><br><br><br><br><br><br><br><br><br>
     <div id="i1" style=" width:800px;margin:0 auto; padding: 80pt;border-radius: 30px 30px 30px 30px;">
 <script data-startup="american-bars" src="https://angel.co/javascripts/embed_jobs.js" id="angellist_embed" async></script>
