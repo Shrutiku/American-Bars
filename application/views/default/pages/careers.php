dde993d3f1b52e321f7538f543902f2a8356cb7a
Shrutiku <shrutikulkarni1311@gmail.com>
2017-03-27 19:09:54 -0700

    c5

--- application/views/default/pages/careers.php
+++ application/views/default/pages/careers.php
@@ -2,7 +2,7 @@
 <style>
     #i1{background:#F5ECCE; border-radius: 30px 30px 30px 30px;}
 </style>
-<div id="i1" style=" width:800px;margin:0 auto; padding: 80pt;border-radius: 30px 30px 30px 30px">
+<div id="i1" style=" width:800px;margin:0 auto; padding: 80pt;border-radius: 30px 30px 30px 30px;background-image:url('C:\Users\SHRUTI\Documents\images');">
 <script data-startup="american-bars" src="https://angel.co/javascripts/embed_jobs.js" id="angellist_embed" async></script>
 </div>
 <br>
