0d8bec1c51cce25bb645098db1b4358739c01c37
Shrutiku <shrutikulkarni1311@gmail.com>
2017-03-27 19:26:32 -0700

    c6

--- application/views/default/pages/careers.php
+++ application/views/default/pages/careers.php
@@ -4,9 +4,12 @@
     .productbar_title mar_top20 padding-10{
         background-image:url('C:\Users\SHRUTI\Documents\images');
     }
+    #i2{ background:#cccccc; padding:100px}
 </style>
+<div id="i2">
 <div id="i1" style=" width:800px;margin:0 auto; padding: 80pt;border-radius: 30px 30px 30px 30px;">
 <script data-startup="american-bars" src="https://angel.co/javascripts/embed_jobs.js" id="angellist_embed" async></script>
+</div>
 </div>
 <br>
 </br>
