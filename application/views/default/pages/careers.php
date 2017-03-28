9054ea03020a476d43001b549c195418ac416885
Shrutiku <shrutikulkarni1311@gmail.com>
2017-03-27 18:21:50 -0700

    bcolor

--- application/views/default/pages/careers.php
+++ application/views/default/pages/careers.php
@@ -1,6 +1,7 @@
 <h1 class="productbar_title mar_top20 padding-10">Our Open Positions:</h1>
-<div style="padding: 100px">
+<style>
+    #i1{background-color: powderblue;}
+</style>
+<div id="i1"style="padding: 100px">
 <script data-startup="american-bars" src="https://angel.co/javascripts/embed_jobs.js" id="angellist_embed" async></script>
 </div>
-<div></div>
-<div></div>
