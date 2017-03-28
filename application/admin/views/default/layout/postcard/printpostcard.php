8ebc1720bd3e64c7de9a56e4a934fa2218813a7c
Anthony <anthony@americanbars.com>
2017-03-27 19:11:41 -0700

    HTML Postcard

--- application/admin/views/default/layout/postcard/printpostcard.php
+++ application/admin/views/default/layout/postcard/printpostcard.php
@@ -96,7 +96,7 @@ $barowner = '';
 
 ?>
 
-<div style="width:6in; height:4in; font-family: Calibri; color: #000; overflow: hidden;  font-size: 11px;letter-spacing: .8px; transform: rotate(90deg) scale(1); margin-top: 1in; margin-left: -1in;">
+<div style="width:5.75in; height:4in; font-family: Calibri; color: #000; overflow: hidden;  font-size: 11px;letter-spacing: .8px; transform: rotate(90deg) scale(1); margin-top: 1in; margin-left: -1in;">
 
   <div  style="width:3.25in; float: left;">
     <div style="float: left; padding-left: 6.25%; padding-top: 31.125%">
