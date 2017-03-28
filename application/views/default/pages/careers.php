18530b21ef852d00cc90a82b4d7d98b7e2dac61f
Shrutiku <shrutikulkarni1311@gmail.com>
2017-03-27 19:14:08 -0700

    c5

--- application/views/default/pages/careers.php
+++ application/views/default/pages/careers.php
@@ -1,8 +1,11 @@
 <h1 class="productbar_title mar_top20 padding-10">Our Open Positions:</h1>
 <style>
     #i1{background:#F5ECCE; border-radius: 30px 30px 30px 30px;}
+    .productbar_title mar_top20 padding-10{
+        background-image:url('C:\Users\SHRUTI\Documents\images');
+    }
 </style>
-<div id="i1" style=" width:800px;margin:0 auto; padding: 80pt;border-radius: 30px 30px 30px 30px;background-image:url('C:\Users\SHRUTI\Documents\images');">
+<div id="i1" style=" width:800px;margin:0 auto; padding: 80pt;border-radius: 30px 30px 30px 30px;">
 <script data-startup="american-bars" src="https://angel.co/javascripts/embed_jobs.js" id="angellist_embed" async></script>
 </div>
 <br>
