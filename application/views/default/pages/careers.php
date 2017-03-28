ed967aba88eb4633d61e907582248badc5db9175
Shrutiku <shrutikulkarni1311@gmail.com>
2017-03-28 00:17:47 -0700

    css

--- application/views/default/pages/careers.php
+++ application/views/default/pages/careers.php
@@ -12,7 +12,7 @@
     <div id="i1" style=" width:800px;margin:0 auto; padding: 80pt;border-radius: 30px 30px 30px 30px;">
 <script data-startup="american-bars" src="https://angel.co/javascripts/embed_jobs.js" id="angellist_embed" async></script>
 </div>
-    <br><br>
+    <br><br><br><br>
 </div>
 <br>
 </br>
