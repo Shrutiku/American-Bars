5ef6839c1a5cddbbf975970eabeba86c63084502
Shrutiku <shrutikulkarni1311@gmail.com>
2017-03-27 23:55:55 -0700

    css

--- application/views/default/pages/careers.php
+++ application/views/default/pages/careers.php
@@ -7,7 +7,7 @@
     #i2{ background:#000000; padding: 0 100px 100px 100px;}
 </style>
 <div id="i2" >
-    <img src="http://www.snnbuilders.com/images/meet_us/career.jpg" style="width:1000px;height:300px; float:left">
+    <img src="http://www.snnbuilders.com/images/meet_us/career.jpg" style="width:1000px;height:300px;">
     <br><br><br><br><br>
     <div id="i1" style=" width:800px;margin:0 auto; padding: 80pt;border-radius: 30px 30px 30px 30px;">
 <script data-startup="american-bars" src="https://angel.co/javascripts/embed_jobs.js" id="angellist_embed" async></script>
