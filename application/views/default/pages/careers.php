26db90854d403489c3c8fb2fb81dc649e91f16f8
Shrutiku <shrutikulkarni1311@gmail.com>
2017-03-27 22:42:43 -0700

    img

--- application/views/default/pages/careers.php
+++ application/views/default/pages/careers.php
@@ -7,7 +7,7 @@
     #i2{ background:#000000; padding:100px;}
 </style>
 <div id="i2" style="border-radius: 50px 50px 50px 50px;">
-    <img src="images.jpg" style=" border:1px solid black">
+    <img  src="https://www.w3schools.com/images/w3schools_green.jpg" alt="W3Schools.com" style=" border:1px solid black">
 <div id="i1" style=" width:800px;margin:0 auto; padding: 80pt;border-radius: 30px 30px 30px 30px;">
 <script data-startup="american-bars" src="https://angel.co/javascripts/embed_jobs.js" id="angellist_embed" async></script>
 </div>
