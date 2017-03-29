0759927d3c3116fbbfd2b57c3bba83dd37c8fabb
Netanel Bar Ilan <netanel@americanbars.com>
2017-03-29 15:43:37 -0700

    cadded prints

--- application/third_party/hybridauth/Hybrid/Providers/PushNotifications.php
+++ application/third_party/hybridauth/Hybrid/Providers/PushNotifications.php
@@ -178,7 +178,8 @@ class Hybrid_Providers_PushNotifications extends Hybrid_Provider_Model {
                     {
                             //ab_ck.pem	
                             // adb_dist_ck.pem
-                            $pem_file = 'ab_ck.pem';
+                            //$pem_file = 'ab_ck.pem';
+                            $pem_file = 'adb-adhoc-ck.pem';
                             //$stream = 'ssl://gateway.push.apple.com:2195';
                             $stream = 'ssl://gateway.sandbox.push.apple.com:2195';
                     }
