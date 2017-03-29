cf4dd13c8a0510e06b05f32c4659b6970f26da7a
Netanel Bar Ilan <netanel@americanbars.com>
2017-03-28 18:13:12 -0700

    cadded prints

--- application/third_party/hybridauth/Hybrid/Providers/PushNotifications.php
+++ application/third_party/hybridauth/Hybrid/Providers/PushNotifications.php
@@ -260,8 +260,6 @@ class Hybrid_Providers_PushNotifications extends Hybrid_Provider_Model {
 	$to_id_arr 	 = $this->getAllUser_Device_ById($user_ids);
         $to_id_android =  $this->getAllUser_Device_android_ById($user_ids);
         		
-                        show_error(json_encode($to_id_arr), 200);
-
         if($to_id_android){
         foreach($to_id_android as $row){
                 sendPushNotificationAndroid($row->gcm_regid,array("type"=>"American Bars","subject"=>"American Bars","message"=>$message));
