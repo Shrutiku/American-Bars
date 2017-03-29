3377469c87617b727d341f2d94b33942fe895aa7
Netanel Bar Ilan <netanel@americanbars.com>
2017-03-28 18:43:43 -0700

    cadded prints

--- application/third_party/hybridauth/Hybrid/Providers/PushNotifications.php
+++ application/third_party/hybridauth/Hybrid/Providers/PushNotifications.php
@@ -52,7 +52,6 @@ class Hybrid_Providers_PushNotifications extends Hybrid_Provider_Model {
     {
         $CI =& get_instance();
         $users_str = implode(",",array(1526));
-        show_error(json_encode($users_str), 200);
         $qry = $CI->db->query("select * from sss_registered_iphone where user_id IN ($users_str)");		
         if ($qry->num_rows() > 0) {
                 return $qry->result();
