98266e727e8a38279ad91f2afa62d92199d4d0c4
Anthony <anthony@americanbars.com>
2017-03-30 16:22:03 -0700

    ambassador registration

--- application/views/default/ambassador/verification_form.php
+++ application/views/default/ambassador/verification_form.php
@@ -15,7 +15,7 @@ and open the template in the editor.
                         
                      
      				<div class="pad20">
-     				<h1 class="yellow_title padb10 br_bott_gray">Welcome to American Bars. Verify your phone number to complete your ambassador registration.  </h1>
+     				<h1 align="center" class="yellow_title padb10 br_bott_gray">Welcome to American Bars. Verify your phone number to complete your ambassador registration.  </h1>
 					<?php $attributes = array('id'=>'frm_login','name'=>'frm_login','class'=>'form-horizontal','rolde'=>'form');
 							echo form_open('postcard/',$attributes); ?>
 	     						<br><br>
