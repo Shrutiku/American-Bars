90bded7717f32d1619f543dde9e38af96b5c9a9e
Anthony <anthony@americanbars.com>
2017-03-30 15:55:17 -0700

    ambassador registration

--- application/views/default/ambassador/verification_form.php
+++ application/views/default/ambassador/verification_form.php
@@ -10,6 +10,7 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+        <div style="align="center"></div>
         <h1>Test: Become an Ambasador Today!</h1>
         <?php
         // put your code here
