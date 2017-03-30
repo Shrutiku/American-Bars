e6b3f9dd5374de9e382d2730d41d4433aca43197
Anthony <anthony@americanbars.com>
2017-03-30 15:41:23 -0700

    ambassador registration

--- application/controllers/ambassador.php
+++ application/controllers/ambassador.php
@@ -16,10 +16,36 @@ class Ambassador extends SPACULLUS_Controller {
     
     function Abassador () {
 
+        
+        parent :: __construct ();
+        $this->load->library ("PasswordHash");
+        $this->load->library ("encrypt");
+        $this->load->model ('home_model');
 
     }
 
     public function index () {
+        $data['error'] = '';
+        $data["active_menu"]='';
+        $data['site_setting'] = site_setting ();
+
+        $theme = getThemeName ();
+        $this->template->set_master_template ($theme.'/template.php');
+
+        $page_detail=meta_setting();
+        $pageTitle=$page_detail->title;
+        $metaDescription=$page_detail->meta_description;
+        $metaKeyword=$page_detail->meta_keyword;
+
+        $this->template->write ('pageTitle', $pageTitle, TRUE);
+        $this->template->write ('metaDescription', $metaDescription, TRUE);
+        $this->template->write ('metaKeyword', $metaKeyword, TRUE);
+        $this->load->library ('form_validation');
+
+        $this->template->write_view ('header', $theme.'/common/header_home', $data, TRUE);
+        $this->template->write_view ('content_center', $theme.'/home/default_postcard', $data, TRUE);
+        $this->template->write_view ('footer', $theme.'/common/footer', $data, TRUE);
+        $this->template->render();
 
     }
 }
