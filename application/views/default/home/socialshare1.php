0b5e20d7986c4d09b3e74b7b604c41ca3c6c8d47
Netanel Bar Ilan <netanel@americanbars.com>
2017-03-29 18:17:52 -0700

    cadded prints

--- application/views/default/home/socialshare1.php
+++ application/views/default/home/socialshare1.php
@@ -74,13 +74,29 @@ function choosepage(provider, accounts) {
 // Output the enabled services and change link/button if the user is authenticated.
 $this->load->helper('url');
 foreach ($providers as $provider => $data) {
-    if (!@$providers['PushNotifications']['connected'] || $provider == 'PushNotifications') {
-        if ($data['connected'] && ($provider == 'PushNotifications' || ($provider != 'PushNotifications' && !@$providers['PushNotifications']['connected']))) {        
-            echo anchor('hauth/logout/' . $provider, img(array('src'=>"$theme_url/images/logout_$provider.png",'border'=>'0','alt'=>'$provider', 'style'=>'max-width:8%;
-       max-height:8%;padding-right: 5px;', 'class' => 'connected')));
+        if ($data['connected']) {    
+            if ($provider != "PushNotifications" && 
+                $providers["PushNotifications"]['connected']) {
+                    echo img(array('src'=>"$theme_url/images/logout_$provider.png",'border'=>'0','alt'=>'$provider', 'style'=>'max-width:8%;
+               max-height:8%;padding-right: 5px;', 'class' => 'connected'));
+            } else {
+                echo anchor('hauth/logout/' . $provider, img(array('src'=>"$theme_url/images/logout_$provider.png",'border'=>'0','alt'=>'$provider', 'style'=>'max-width:8%;
+           max-height:8%;padding-right: 5px;', 'class' => 'connected')));
+            }
         } else {
-            echo anchor('hauth/login/' . $provider, img(array('src'=>"$theme_url/images/login_$provider.png",'border'=>'0','alt'=>'$provider', 'style'=>'max-width:8%;
-       max-height:8%;padding-right: 5px;', 'class' => 'login')));
+            if ($provider == "PushNotifications" && 
+                ($providers["Facebook"]['connected'] || 
+                $providers["Twitter"]['connected'] || 
+                $providers["Google"]['connected'] ||
+                $providers["Instagram"]['connected'])) {
+                echo img(array('src'=>"$theme_url/images/login_$provider.png",'border'=>'0','alt'=>'$provider', 'style'=>'max-width:8%;
+max-height:8%;padding-right: 5px;', 'class' => 'login'));
+            }
+            else
+            {
+                echo anchor('hauth/login/' . $provider, img(array('src'=>"$theme_url/images/login_$provider.png",'border'=>'0','alt'=>'$provider', 'style'=>'max-width:8%;
+max-height:8%;padding-right: 5px;', 'class' => 'login')));
+            }
         }
     }
 }
