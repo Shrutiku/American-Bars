8714ec9e186770df94f94ac59567295d531981e6
Anthony <anthony@americanbars.com>
2017-03-30 17:08:54 -0700

    ambassador registration

--- application/views/default/ambassador/verification_form.php
+++ application/views/default/ambassador/verification_form.php
@@ -16,22 +16,22 @@ and open the template in the editor.
                      
      				<div class="pad20">
                                     <h1 align="center" class="yellow_title padb10 br_bott_gray">Welcome to American Bars. Verify your phone number to complete your ambassador registration.</h1>
-					<?php $attributes = array('id'=>'frm_login','name'=>'frm_login','class'=>'form-horizontal','rolde'=>'form');
-							echo form_open('ambassador/',$attributes); ?>
-	     						<br><br>
-	     				<?php if($error!=""){ ?>
-                                        <div class="error1 text-center"><a class="closemsg" data-dismiss="alert"></a><span><?php echo $error; ?></span></div>
-                        <?php }?>
+                                    <?php $attributes = array('id'=>'frm_login','name'=>'frm_login','class'=>'form-horizontal','rolde'=>'form');
+                                                    echo form_open('ambassador/',$attributes); ?>
+                                                    <br><br>
+                                    <?php if($error!=""){ ?>
+                                    <div class="error1 text-center"><a class="closemsg" data-dismiss="alert"></a><span><?php echo $error; ?></span></div>
+                                    <?php }?>
    
-                                        <div class="padtb8">
-                                               <div class="col-sm-3">
-                                                       <label class="control-label">Phone Number :</label>
-                                               </div>
-	                       		<div class="input_box col-sm-7">
-	                           		<input type="text" class="form-control form-pad" id="phone_num" placeholder="310 555 1234" name="phone_num">
-	                       		</div>
-	                       		<div class="clearfix"></div>
-                                        </div>
+                                    <div class="padtb8">
+                                           <div class="col-sm-3">
+                                                   <label class="control-label">Phone Number :</label>
+                                           </div>
+                                    <div class="input_box col-sm-7">
+                                            <input type="text" class="form-control form-pad" id="phone_num" placeholder="310 555 1234" name="phone_num">
+                                    </div>
+                                    <div class="clearfix"></div>
+                                    </div>
                                     <div class="padtb8">
 	                       		<div class="col-sm-3"></div>
 	                       		<div class="col-sm-7">
@@ -39,9 +39,9 @@ and open the template in the editor.
                                             <div class="mar_top4">
                                                 <button class="btn btn-lg btn-primary"  type="submit" name="submit"  id="submit" />Enter</button>
                                             </div>
-		        					<div class="clearfix"></div>
+                                            <div class="clearfix"></div>
 		        		</div>
-		        				<div class="clearfix"></div>
+                                        <div class="clearfix"></div>
                                     </div>	                       
 	        		</div>
      			</div>
