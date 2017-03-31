cb4f7d2aa689b84d3bc2264d0424f88e401c81c9
Anthony <anthony@americanbars.com>
2017-03-30 18:43:45 -0700

    ambassador registration

--- application/views/default/ambassador/verification_form.php
+++ application/views/default/ambassador/verification_form.php
@@ -37,14 +37,28 @@ and open the template in the editor.
 	                       		<div class="col-sm-7">
 		                       		
                                             <div class="mar_top4">
-                                                <button class="btn btn-lg btn-primary"  type="submit" name="submit"  id="submit" />Enter</button>
+                                                <button class="btn btn-lg btn-primary"  type="submit" name="submit"  id="submit" onclick="showDiv()">Enter</button>
                                             </div>
                                             <div class="clearfix"></div>
 		        		</div>
                                         <div class="clearfix"></div>
-                                    </div>	                       
+                                    </div>
+                                    <div id="showdiv" class="padtb8" style="display:none;">
+                                           <div class="col-sm-3">
+                                                   <label class="control-label">Phone Number :</label>
+                                           </div>
+                                    <div class="input_box col-sm-7">
+                                            <input type="text" class="form-control form-pad" id="phone_num" placeholder="310 555 1234" name="phone_num">
+                                    </div>
+                                    <div class="clearfix"></div>
+                                    </div>
 	        		</div>
      			</div>
      		</div>
    		</div>
    	</div>
+<script>
+function showDiv() {
+   document.getElementById('welcomeDiv').style.display = "block";
+}
+    </script>
