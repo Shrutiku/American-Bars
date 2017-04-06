843bb054fced81eab84a4953b0c39aa2892e94c1
Shrutiku <shrutikulkarni1311@gmail.com>
2017-04-06 15:34:29 -0700

    c1

--- application/views/default/pages/careers.php
+++ application/views/default/pages/careers.php
@@ -8,7 +8,7 @@
 </style>
 <div id="i2" >
     <img src="http://www.snnbuilders.com/images/meet_us/career.jpg" style="width:1260px;height:300px;">
-    <br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br>
     <div id="i1" style=" width:800px;margin:0 auto; padding: 80pt;border-radius: 30px 30px 30px 30px;">
 <script data-startup="american-bars" src="https://angel.co/javascripts/embed_jobs.js" id="angellist_embed" async></script>
 </div>
