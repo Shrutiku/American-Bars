8a773c2dea4aadb3f07d8bbdaea62815e882b460
Netanel Bar Ilan <netanel@americanbars.com>
2017-05-08 22:36:46 -0700

    added one form to add for all drink

--- application/views/default/bar/bar_cocktail_add.php
+++ application/views/default/bar/bar_cocktail_add.php
@@ -46,7 +46,7 @@
 	                       		<div class="col-sm-3"></div>
 	                       		<div class="col-sm-7 mart10 text-left">
 	                       			<button type="submit" class="btn btn-lg btn-primary marr_10" >Save</button> 
-	                       			<a  class="btn btn-lg btn-primary marr_10" href="<?php echo site_url('bar/bar_cocktail');?>" >Cancel</a>
+	                       			<a  class="btn btn-lg btn-primary marr_10" href="<?php echo site_url('bar/add_drink');?>" >Cancel</a>
 	                       		</div>
 	                       		<div class="clearfix"></div>
 	                       	</div>
@@ -107,7 +107,7 @@ $(document).ready(function(){
         list_add();
 	 });
         
-        $('#form').validate(
+        /*$('#form').validate(
 		{
 		rules: {
 					'cocktail_id[]': {
@@ -175,7 +175,7 @@ $(document).ready(function(){
 		   		 }
 		    });
 		  }
-		})
+		})*/
 		
 		
     function getData()
