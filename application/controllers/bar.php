19497047c7d64f380194407cdba2de3e2a5c337c
Netanel Bar Ilan <netanel@americanbars.com>
2017-05-08 22:19:16 -0700

    added one form to add for all drink

--- application/controllers/bar.php
+++ application/controllers/bar.php
@@ -2235,6 +2235,87 @@ function getallliquorbybar_new()
 		}
 	}
 
+        function choose_cocktail($limit=10,$keyword='1V1',$offset=0,$msg='')
+	{
+		if($this->session->userdata('user_type')!='bar_owner')
+		{
+			redirect('home');
+		}
+		if(get_authenticateUserID()=='')
+		{
+			redirect('home');
+		}
+        
+		$getbarinfo = getBarInfoByUserID(get_authenticateUserID());
+		
+		$data = array();
+		$data['msg'] = $msg;
+		$theme = getThemeName ();
+		$this->template->set_master_template ($theme.'/template.php');
+		$this->load->library('pagination');
+		$page_detail=meta_setting();
+		$pageTitle=$page_detail->title;
+		$metaDescription=$page_detail->meta_description;
+		$metaKeyword=$page_detail->meta_keyword;
+		
+		$data['error'] = '';
+		$data['site_setting'] = site_setting ();
+        $data['active_menu']='home';
+        $this->template->write ('pageTitle', $pageTitle, TRUE);
+		$this->template->write ('metaDescription', $metaDescription, TRUE);
+		$this->template->write ('metaKeyword', $metaKeyword, TRUE);
+        $data['getbar'] = $this->home_model->get_bar_info(get_authenticateUserID());
+		
+		if($this->input->post('event_keyword')!='')
+		{
+			$keyword= $this->input->post('event_keyword');
+			$limit= $this->input->post('limit');
+			$offset= $this->input->post('offset');
+		}
+		else {
+			$keyword = $keyword;
+			$limit= $limit;
+			$offset= $offset;
+		}
+		$config['uri_segment']='5';
+		$config['base_url'] = base_url().'bar/bar_cocktail/'.$limit.'/'.$keyword;
+		$config["total_rows"] = $this->bar_model->getBarCocktailcount(@$data['getbar']['bar_id'],$keyword);
+		
+		//$config['total_rows'] = $this->service_provider_model->countStaff($id,$keyword);
+		//echo $data["total_rows"];
+		$config['per_page'] = $limit;
+		$this->pagination->initialize($config);	
+		$data['page_link'] = $this->pagination->create_links();
+		$data['result'] = $this->bar_model->getBarCocktailDetail(@$data['getbar']['bar_id'],$offset,$limit,$keyword);
+		
+	
+		//	$data['result'] = $this->bar_model->getAllComments($bar_id,$offset,$limit);	
+		$data['offset'] = $offset;
+		$data['limit'] = $limit;
+		$data['redirect_page']='bar_cocktail';
+		
+		if($this->input->is_ajax_request()){
+			echo $this->load->view($theme .'/bar/bar_cocktail_ajax',$data,TRUE);die;
+			
+		}
+		else {
+		$this->template->write_view ('header', $theme.'/common/header', $data, TRUE);
+			
+			//echo $getbarinfo->serve_as;
+		if($getbarinfo->serve_as=='liquor')
+		{
+			$this->template->write_view ('content_center', $theme.'/bar/bar_cocktail_no', $data, TRUE);
+		}
+		else
+	    {
+	    	
+	       $this->template->write_view ('content_center', $theme.'/bar/bar_cocktail', $data, TRUE);	
+	    }		
+		$this->template->write_view ('footer', $theme.'/common/footer', $data, TRUE);
+		$this->template->render ();
+		}
+	}
+        
     function bar_liquor($limit=10,$keyword='1V1',$offset=0,$msg='')
 	{
 		if($this->session->userdata('user_type')!='bar_owner')
@@ -2314,7 +2395,7 @@ function getallliquorbybar_new()
 		}
 	}
 
-    function add_cocktail($bar_id='')
+    function add_cocktail($bar_id='', $redirect='')
 	{
 		if($this->session->userdata('user_type')!='bar_owner')
 		{
@@ -2355,6 +2436,10 @@ function getallliquorbybar_new()
 			
 			
 		}
+                
+                if($redirect == "bar_cocktail") {
+                    redirect("bar/bar_cocktail");
+                }
 		
 			echo json_encode($response);
 		    die;
--- application/views/default/bar/bar_cocktail_add.php
+++ application/views/default/bar/bar_cocktail_add.php
@@ -1,3 +1,4 @@
+
 <script type="text/javascript" src="<?php echo base_url().getThemeName(); ?>/plugin/Tokenize/jquery.tokenize.js"></script>
 <link rel="stylesheet" type="text/css" href="<?php echo base_url().getThemeName(); ?>/plugin/Tokenize/jquery.tokenize.css" />
 
@@ -10,14 +11,21 @@
 					   <?php echo $this->load->view(getThemeName().'/bar/cocktail_suggest');?>
 </div>	
 <div class="wrapper row6 padtb10 has-js">
-<div class="container">	
-<div id="list_show">	
+     	<div class="container">
+     		<div class="margin-top-50 bg_brown">
+     			<?php echo $this->load->view(getThemeName().'/home/dashboard_menu'); ?>
+     			<div class="dashboard_detail">
+     				<div class="result_search event"><div class="result_search_text"><i class="strip cocktails"></i> Cocktails</div></div>
+		     		<div class="dashboard_subblock">
+		     			<div>
+		     		
+				<div id="list_show">	
 					<input type="hidden" name="redirect_page" id="redirect_page" value="<?php echo $redirect_page;?>"/>
 					<input type="hidden" name="offset" id="offset" value="<?php echo ($offset!='')?$offset:0; ?>" />
 					<input type="hidden" name="limit" id="limit" value="<?php echo ($limit>0)?$limit:10; ?>" />
 					
 					<div class="error1 hide1 center" id="cm-err-main1">&nbsp;</div>
-					<form name="add_event" id="form" method="post" enctype="multipart/form-data" action="<?php echo site_url('bar/add_cocktail/'.base64_encode($getbar['bar_id'])); ?>">
+					<form name="add_event" id="form" method="post" enctype="multipart/form-data" action="<?php echo site_url('bar/add_cocktail/'.base64_encode($getbar['bar_id'])."/bar_cocktail"); ?>">
 						<!-- <input type="hidden" name="event_id" id="event_id" value="" /> -->
      				
 		     			<div class="text-center pad_t15b20">
@@ -46,5 +54,469 @@
      		</div>
      			</form>
      			</div>
-</div>
-</div>
+     			
+		     	</div>
+     		</div>
+     	<div class="clearfix"></div>
+     </div>
+   </div>
+ </div>
+<script type="text/javascript" src="<?php echo base_url().getThemeName();?>/js/jquery_form.js"></script>
+    <script>
+   
+    function morelink(id)
+    {
+    	  $("#myModalnew_open_"+id).modal('show');
+    }
+$(document).ready(function(){
+	 $('.label_check, .label_radio').click(function(){
+    	        	
+            setupLabel();
+        });
+        setupLabel(); 	
+	  bindJquery();	
+	 var arrVal = new Array();
+	var t = $("[class=user_type]").val();
+	$('.tags').tagsInput({
+		autocomplete_url:'<?php echo site_url('bar/getallcocktailbybar/');?>',
+		itemValue: 'value',
+		itemText: 'text',
+		autocomplete:{
+		   source: function(request, response) {
+		  var t = <?php echo $getbar['bar_id']; ?>;
+			  $.ajax({
+				 url: "<?php echo site_url('bar/getallcocktailbybar');?>",
+				 dataType: "json",
+				 data: {
+				   	utype : t,
+					em: request.term,
+					
+				 },
+				 success: function(data) {
+					response( $.map( data, function( item ) {
+						return {
+							label: item.label,
+							value: item.value
+						}
+					}));
+				}
+			  })
+		   },
+		}
+	});
+        list_add();
+	 });
+        
+        $('#form').validate(
+		{
+		rules: {
+					'cocktail_id[]': {
+							required: true,
+					},
+					
+						errorClass:'error fl_right'
+				},
+				
+		submitHandler: function(form){
+		$(form).ajaxSubmit({
+		type: "POST",
+		   		 dataType : 'json',
+				 beforeSubmit: function() 
+				 {
+		       		$('#dvLoading').fadeIn('slow');
+		    	 },
+		    	
+		    	uploadProgress: function ( event, position, total, percentComplete ) {	
+		        },
+		    
+		    	success : function ( json ) 
+		    	{
+		    		
+					if(json.status == "fail")
+					{
+						$("#cm-err-main1").show();
+					$("#cm-err-main1").html('Cocktail Name Field is required');
+			    		$('#dvLoading').fadeOut('slow');
+			    		scrollToDiv('cm-err-main1');
+				  		// setTimeout(function () 
+						// {
+						      // $("#cm-err-main1").fadeOut('slow');
+						// }, 3000);
+					return false;
+					}
+			
+					else
+					{
+						//alert("sdsa");
+						$("#cm-err-main1").hide();
+						$("#cm-err-main1").html("");
+						if($('#event_id').val()=='')
+						{
+							$.growlUI('Your cocktail add successfully .');
+						}
+						else
+						{
+							$.growlUI('Your cocktail update successfully .');
+						}
+						$(':input','#form')
+					 	.not(':button, :submit, :reset, :hidden')
+					 	.val('')
+					 	//.removeAttr('checked')
+					 	.removeAttr('selected');
+					 	 $("#list_hide").slideDown();
+					 	 $("#list_hide_m").slideDown();
+					     $("#hd_del").slideDown();
+					     $("#hs_del").slideUp();
+					     $('#list_show').slideUp();
+					     $("#at_ajax").remove();
+					     getData();
+					}
+					$('#dvLoading').fadeOut('slow');
+		   		 }
+		    });
+		  }
+		})
+		
+		
+    function getData()
+	{
+	//var keyword=($('#keyword').val()!='')?$('#keyword').val().split(' ').join('-'):'1V1';
+	var limit = $('#limit').val();
+    var keyword = $("#event_keyword").val();
+    if(keyword=='')
+    {
+    	var keyword = '1V1';
+    }
+	var offset = $('#offset').val();
+	var redirect_page=$('#redirect_page').val();
+	var url='<?php echo site_url('bar/') ?>/'+redirect_page+'/'+limit+'/'+keyword+'/'+offset;
+	
+	
+	$.ajax({
+			url : url,
+			cache: false,
+			// beforeSend : function() {
+				// blockUI('.portlet-body');
+			// },
+			  beforeSend : function(){
+			      $('#dvLoading').fadeIn('slow');
+			   },
+			   complete: function(){
+			   
+			     $('#dvLoading').fadeOut('slow');
+			     
+			   },
+			success : function(response) {
+				// alert(response);
+				$('.content').html('');
+				$('.content').html(response);
+				setupLabel();
+				bindJquery();
+				
+				//bindJquery();
+			},
+			// complete : function() {
+				// unblockUI('.portlet-body');
+			// },
+	});
+	
+	}
+    function list_add()
+    {
+    		$("#cm-err-main1").hide();
+						$("#cm-err-main1").html("");
+    	 // $.ajax({
+	       // type: "POST",
+		   // url: "<?php //echo site_url('bar/getallcocktailbybar')?>",
+		   // data : {id:<?php //echo  $getbar['bar_id']; ?>},
+		   // dataType : 'json',
+		   // success: function(response) {
+		   	// $("#cocktail_id").empty();
+		   	    // $(".search-choice").remove();
+		   	    // $('.result-selected').removeClass('result-selected').addClass('active-result');
+		   	    // $('#cocktail_id').html(response.result1);
+		   	    // $('.chzn-results').html(response.result2);
+// 		   
+		     // $(".chosen").each(function () 
+		   	// {
+	            // $(this).chosen({
+	                // allow_single_deselect: $(this).attr("data-with-deselect") == "1" ? true : false
+	            // });
+        	// });
+// 		       
+		      // } 
+	   // });
+    	$("#event_id").val('');
+    	$(':input','#form')
+					 	.not(':button, :submit, :reset, :hidden')
+					 	.val('')
+					 	//.removeAttr('checked')
+					 	.removeAttr('selected');
+		//$("#cocktail_id").val('');
+		
+		$("#cocktail_id_tagsinput span").remove();			 	
+    	$("#list_hide").slideUp();
+    	$("#list_hide_m").slideUp();
+    	$("#hd_del").slideUp();
+    	$("#hs_del").slideDown();
+    	$('#tokenize').data('tokenize').clear();
+    	$('#list_show').slideDown();
+    	
+    }
+    
+    function goto_main()
+    {	$("#cm-err-main1").hide();
+						$("#cm-err-main1").html("");
+    	$("#event_id").val('');
+    	$(':input','#form')
+					 	.not(':button, :submit, :reset, :hidden')
+					 	.val('')
+					 	//.removeAttr('checked')
+					 	.removeAttr('selected');
+		$("#img_here").removeAttr('src');			 	
+    	$("#list_hide").slideDown();
+    	$("#list_hide_m").slideDown();
+    	$("#hd_del").slideDown();
+    	$("#hs_del").slideUp();
+    	$('#list_show').slideUp();
+    }
+</script>
+
+<script>
+	
+	 $(".pagination li a").click(function() {
+		  //alert("Handler for .click() called.");
+		  var res = $.ajax(
+	        {						
+			   type: 'POST',
+			   url: $(this).attr("href"),
+			   dataType: 'post', 
+			   cache: false,
+			   async: false,
+			   beforeSend : function(){
+			      $('#dvLoading').fadeIn('slow');
+			   },
+			   complete: function(){
+			   
+			     $('#dvLoading').fadeOut('slow');
+			     
+			    }
+			}).responseText;
+			
+			$("#list_hide").html(res);
+			bindJquery();
+			setupLabel();	
+			
+			return false;
+			
+		});
+ function editbarevent(id)
+ {	$("#cm-err-main1").hide();
+						$("#cm-err-main1").html("");
+ 		
+ 	 $.ajax({
+	       type: "POST",
+		   url: "<?php echo site_url('bar/bareventdetail')?>",
+		   data : {id:id},
+		   dataType : 'JSON',
+		   success: function(response) {
+		   
+		      $("#event_id").val(response.event_id);
+		      $("#event_title").val(response.event_title);
+		      $("#event_desc").val(response.event_desc);
+		      $("#start_date").val(response.start_date);
+		      $("#end_date").val(response.end_date);
+		      $("#address").val(response.address);
+		      $("#city").val(response.city);
+		      $("#state").val(response.state);
+		      $("#phone").val(response.phone);
+		      $("#zipcode").val(response.zipcode);
+		      $("#event_video_link").val(response.event_video_link);
+		      $("#is_power_boost_event").val(response.is_power_boost_event);
+		      $("#status").val(response.status);
+		      $("#prev_event_image").val(response.event_image);
+		      $("#prev_event_video").val(response.event_video);
+		      
+		      if(response.event_video!='')
+		      {
+		      		 var src_vid = '<?php echo base_url().'upload/event_video/'?>';
+		      	     var htm = '<a href="'+src_vid+response.event_video+'" id="video_add" class="image_play fancybox-video">'+response.event_video+'</a>';
+		      	   //  $("#video_add").attr("href", src_vid+response.event_video);
+		      		 $("#prev_event_video_htm").html(htm);
+			  }
+		     
+		      if(response.event_image!='')
+		      {
+		      		var src1 = '<?php echo base_url().'upload/event_thumb/'?>';
+					$("#img_here").attr("src", src1+response.event_image);
+			 }
+		      $("#list_hide").slideUp();
+	    	$("#list_hide_m").slideUp();
+	    	$("#hd_del").slideUp();
+	    	$("#hs_del").slideDown();
+	    	
+	    	$('#list_show').slideDown();
+	    	bindJquery();
+		     
+		  }
+	   });
+ }
+ function get_search_event()
+ {
+ 	  var event_keyword = $("#event_keyword").val();
+ 	  var limit = $("#limit").val();
+ 	  var offset = 0; 
+ 	  var res = $.ajax(
+	        {						
+			   type: 'POST',
+			   url: '<?php echo site_url('bar/bar_cocktail/')?>',
+			   dataType: 'post', 
+			   data : {event_keyword:event_keyword,limit:limit,offset:offset},
+			   cache: false,
+			   async: false,
+			   beforeSend : function(){
+			      $('#dvLoading').fadeIn('slow');
+			   },
+			   complete: function(){
+			   
+			     $('#dvLoading').fadeOut('slow');
+			     
+			    }
+			}).responseText;
+			
+			$("#list_hide").html(res);
+			    setupLabel();
+				bindJquery();
+ }
+ 
+ function deletecocktail(id)
+ {
+ 	 
+			alertify.confirm("Are you sure you want to delete this cocktail?", function (e) {
+				if (e) {
+					 var res = $.ajax(
+	        {						
+			   type: 'POST',
+			   url: '<?php echo site_url('bar/barcocktaildelete/')?>',
+			   dataType: 'post', 
+			   data : {id:id},
+			   cache: false,
+			   async: false,
+			   beforeSend : function(){
+			      $('#dvLoading').fadeIn('slow');
+			   },
+				complete: function(){
+				     $('#dvLoading').fadeOut('slow');
+				     getData();
+				     $("#remove_event_"+id).remove();
+				     $.growlUI('Your cocktail deleted successfully .');
+				    }
+				}).responseText;
+				bindJquery();
+			}
+			return false;
+			
+ });
+ }
+ 
+ function bindJquery()
+	{
+		
+		
+		jQuery('.group-checkable').change(function () {
+			
+	                if ($('.label_check input').length) {
+			            $('.label_check').each(function(){ 
+			                $(this).removeClass('c_on');
+			                            $('.checkboxes').removeAttr('Checked'); 
+			            });
+			            $('.label_check input:checked').each(function(){ 
+			            	
+			               // $(this).parent('label').addClass('c_on');
+			                $( ".radio-checkbox" ).addClass( "c_on" ); 
+			                            $('.checkboxes').attr('Checked','Checked'); 
+			                  //  $('#states').find('span').addClass('checked');        
+			            });                
+			        };
+	            });
+	
+	}
+
+ function setupLabel() {
+        if ($('.label_check input').length) {
+            $('.label_check').each(function(){ 
+                $(this).removeClass('c_on');
+            });
+            $('.label_check input:checked').each(function(){ 
+                $(this).parent('label').addClass('c_on');
+            });                
+        };
+        if ($('.label_radio input').length) {
+            $('.label_radio').each(function(){ 
+                $(this).removeClass('r_on');
+            });
+            $('.label_radio input:checked').each(function(){ 
+                $(this).parent('label').addClass('r_on');
+            });
+        };
+    };
+    
+    function setaction(elename, actionval, formname) {
+
+	vchkcnt=0;
+	elem = document.getElementsByName(elename);
+	
+	
+	for(i=0;i<elem.length;i++){
+		if(elem[i].checked) vchkcnt++;
+		//vchkcnt++;
+			
+	}
+	if(vchkcnt==0) {
+			alertify.alert("Please select a record .");
+			return false;
+	} else {
+		
+		alertify.confirm("Are you sure you want to delete this cocktail?", function (e) {
+				if (e) {
+			document.getElementById('action').value=actionval;	
+			//$('#frm_admin').submit();
+		var $form = $('#actionevent');
+        var $target = $($form.attr('data-target'));
+ 		var limit=$('#limit').val();
+ 		var offset=$('#offset').val();
+ 		var keyword=$('#event_keyword').val();
+        $.ajax({
+            type: $form.attr('method'),
+            url: $form.attr('action'),
+            cache: false,
+            dataType:'json',
+            data: $form.serialize(),
+            beforeSend : function() {
+				$('#dvLoading').fadeIn('slow');
+			},success: function(res, status) {
+				// alert(res);
+                if(res.status=='done'){
+                	$.growlUI('Your cocktail deleted successfully .'); 
+                	getData();	
+                }
+                
+            },complete : function() {
+				$('#dvLoading').fadeOut('slow');
+			},
+        });
+		}		
+		else
+		{
+			return false;
+		}
+		});
+	}
+}
+var base_url = "<?php echo base_url();?>";
+ $('#tokenize').tokenize({
+	  // datas: '<?php //echo base_url(); ?>+"advertisement/getAllCityOrZipcode/city/"'
+	     datas: ""+base_url+"bar/getallcocktailbybar_new/?bar_id=<?php echo $getbar['bar_id'];?>/"
+	});
+</script>
+
