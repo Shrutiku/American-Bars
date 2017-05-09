de59795c9c55eade3164515265d560f3a02b6068
Netanel Bar Ilan <netanel@americanbars.com>
2017-05-08 21:42:17 -0700

    added one form to add for all drink

--- application/views/default/bar/bar_add_drink.php
+++ application/views/default/bar/bar_add_drink.php
@@ -1,4 +1,17 @@
+<script type="text/javascript" src="<?php echo base_url().getThemeName(); ?>/plugin/Tokenize/jquery.tokenize.js"></script>
+<link rel="stylesheet" type="text/css" href="<?php echo base_url().getThemeName(); ?>/plugin/Tokenize/jquery.tokenize.css" />
 
+
+<script>
+	 var test = '<a href="#suggestmodal" data-toggle="modal" class="yellowlink">Suggest New Cocktail</a>';
+	
+</script>
+<div class="modal fade login_pop2" id="suggestmodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" >
+					   <?php echo $this->load->view(getThemeName().'/bar/cocktail_suggest');?>
+</div>	
+<div class="wrapper row6 padtb10 has-js">
+<div class="container">	
+<div id="list_show">	
 <div class="wrapper row6 padtb10 has-js">
      	<div class="container">
             <div class='pull-left' style="text-align: center;"><div class="result_search_text">What kind of drink you want to add?</div></div>
@@ -8,4 +21,6 @@
                     <a class="btn btn-lg btn-primary marr_10" href="<?php echo site_url('/bar/bar_liquor');?>">Liquor</a>
                 </div>
         </div>
+</div>
+            </div>
 </div>
