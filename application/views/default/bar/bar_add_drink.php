5b7b68d634a7174570d815bcb57353867b812a07
Netanel Bar Ilan <netanel@americanbars.com>
2017-05-08 21:36:39 -0700

    added one form to add for all drink

--- application/views/default/bar/bar_add_drink.php
+++ application/views/default/bar/bar_add_drink.php
@@ -1,7 +1,7 @@
 
 <div class="wrapper row6 padtb10 has-js">
      	<div class="container">
-            <p>What kind of drink you want to add?</p>
+            <div class='pull-left'><div class="result_search_text">What kind of drink you want to add?</div></div>
      		<div class="margin-top-50 bg_brown">
                     <a class="btn btn-lg btn-primary marr_10"  href="<?php echo site_url('/bar/bar_cocktail');?>">Cocktail</a>
                     <a class="btn btn-lg btn-primary marr_10" href="<?php echo site_url('/bar/bar_beer');?>">Beer</a>
