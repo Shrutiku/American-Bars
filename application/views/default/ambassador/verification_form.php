5bf612508002e7a1304d7925cbec588b38749dc3
Anthony <anthony@americanbars.com>
2017-05-09 13:48:45 -0700

    ambassador page

--- application/views/default/ambassador/verification_form.php
+++ application/views/default/ambassador/verification_form.php
@@ -31,14 +31,14 @@ and open the template in the editor.
                                 <p class="bar_add yellow_title">We'll send an activation code via SMS to your mobile phone.</p>
 
                                 <div class="clearfix"></div>
-                                <div class="input box col-sm-3" style="text-align: center;"></div>
-                                <div class="input box col-sm-3" style="text-align: center;">
+                                <div class="input box col-sm-4" style="text-align: center;"></div>
+                                <div class="input box col-sm-2" style="text-align: center;">
                                     <input type="text" class="form-control form-pad" id="phone_number" name="phone_number" style="width:150px">
                                 </div>
-                                <div class="col-sm-3"> 
+                                <div class="col-sm-2" style="text-align: left"> 
                                     <button class="btn btn-lg btn-primary"  type="submit" name="submit"  id="submit" />Verify</button>
                                 </div>
-                                <div class="input box col-sm-3" style="text-align: left;"></div>
+                                <div class="input box col-sm-4" style="text-align: left;"></div>
                                     <div class="clearfix"></div>  
                                 </div>
                             </div>
