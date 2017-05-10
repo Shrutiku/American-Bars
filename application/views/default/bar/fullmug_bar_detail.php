3e3c7479a77307418b0c059f54ac0fe5d91b126b
Carleen <cpetrosi@lion.lmu.edu>
2017-05-10 13:13:47 -0700

    testing map

--- application/views/default/bar/fullmug_bar_detail.php
+++ application/views/default/bar/fullmug_bar_detail.php
@@ -1675,25 +1675,25 @@ $(".pagination li a").click(function() {
 			if (geocoder) {
       		geocoder.geocode( { 'address': address}, function(results, status) {
 	        if (status == google.maps.GeocoderStatus.OK) {
-	          if (status != google.maps.GeocoderStatus.ZERO_RESULTS) {
-	          map.setCenter(results[0].geometry.location);
+                    if (status != google.maps.GeocoderStatus.ZERO_RESULTS) {
+                    map.setCenter(results[0].geometry.location);
 
-	            var infowindow = new google.maps.InfoWindow(
-	                { content: '<b>'+address+'</b>',
-	                  size: new google.maps.Size(150,50)
-	                });
+                      var infowindow = new google.maps.InfoWindow(
+                          { content: '<b>'+address+'</b>',
+                            size: new google.maps.Size(150,50)
+                          });
 
-	            var marker = new google.maps.Marker({
-	                position: results[0].geometry.location,
-	                map: map,
-	                title:address
-	            });
-	            google.maps.event.addListener(marker, 'click', function() {
-	                infowindow.open(map,marker);
-	            });
+                      var marker = new google.maps.Marker({
+                          position: results[0].geometry.location,
+                          map: map,
+                          title:address
+                      });
+                      google.maps.event.addListener(marker, 'click', function() {
+                          infowindow.open(map,marker);
+                      });
 
-	          }
-	         }
+                    }
+	        }
           });
         }
 		google.maps.event.trigger(map, 'resize');
