c47a0f966a7d425267183fcf6266d1d7fbc44c2c
Carleen <cpetrosi@lion.lmu.edu>
2017-05-16 12:06:19 -0700

    testing admin

--- application/admin/views/default/layout/common/sidebar.php
+++ application/admin/views/default/layout/common/sidebar.php
@@ -211,8 +211,8 @@ $(document).ready(function(){
 				
 				
 				
-				<li class="menu_tag <?php echo ($uri[1]=='list_suggest_bar' || $uri[1]=='edit_suggest_bar' ||  $uri[1]=='search_list_suggest_bar' || $uri[1]=='view_suggest_bar')?'active':''; ?>">
-					<a href="<?php echo site_url('suggest_bar/list_suggest_bar') ?>" <?php echo (isset($uri[1]) && ($uri[1]=='list_suggest_bar' || $uri[1]=='view_suggest_bar' || $uri[1]=='search_list_suggest_bar'))?'class="active"':''; ?>><span class="badge badge-info"><?php echo get_unread_sugbar();?></span>
+				<li class="menu_tag <?php //echo ($uri[1]=='list_suggest_bar' || $uri[1]=='edit_suggest_bar' ||  $uri[1]=='search_list_suggest_bar' || $uri[1]=='view_suggest_bar')?'active':''; ?>">
+					<a href="<?php echo site_url('suggest_bar/list_suggest_bar') ?>" <?php //echo (isset($uri[1]) && ($uri[1]=='list_suggest_bar' || $uri[1]=='view_suggest_bar' || $uri[1]=='search_list_suggest_bar'))?'class="active"':''; ?>><!--<span class="badge badge-info"><?php //echo get_unread_sugbar();?></span>-->
 						<i class="sidebar_icon suggest_icon"></i>
 						<span class="title"> Suggested Bars</span>
 					</a>
