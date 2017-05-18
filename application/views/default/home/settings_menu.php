3c4d054375714f4b53d2ee803c24579251d1b81d
Carleen <cpetrosi@lion.lmu.edu>
2017-05-18 10:37:31 -0700

    testing dashboard

--- application/views/default/home/settings_menu.php
+++ application/views/default/home/settings_menu.php
@@ -24,32 +24,32 @@
 			     		<div class="pull-right marr_10">
 		     				<!-- <a href="#" class="review">Upgrade to Full Mug</a> -->
 		     				<a class="review" name="" href="<?php echo site_url('home/registration_step3_upgrade/'.base64_encode($getbar['bar_id']).'/fullmug');?>">Upgrade to Full Mug</a>
-			       </div>
-			       <?php } ?>
+                                        </div>
+                                <?php } ?>
 			       
-			       <?php if(($getbar['bar_type']=='half_mug' || $getbar['bar_type']=='full_mug' && $getbar['is_managed']=='no') ){?>
+                                <?php if(($getbar['bar_type']=='half_mug' || $getbar['bar_type']=='full_mug' && $getbar['is_managed']=='no') ){?>
 			     		<div class="pull-right marr_10">
-		     				<!-- <a href="#" class="review">Upgrade to Full Mug</a> -->
-		     				<a class="review" name="" href="<?php echo site_url('home/registration_step3_upgrade/'.base64_encode($getbar['bar_id']).'/managed');?>">Upgrade to  Managed Account</a>
-			       </div>
+                                            <!-- <a href="#" class="review">Upgrade to Full Mug</a> -->
+                                            <a class="review" name="" href="<?php echo site_url('home/registration_step3_upgrade/'.base64_encode($getbar['bar_id']).'/managed');?>">Upgrade to  Managed Account</a>
+                                        </div>
 			       <?php } ?>
-			       <div class="clear"></div>
+                                    <div class="clear"></div>
 		     		</div>
 		     		<div class="dashboard_subblock">
 		     			<?php if($getbar['bar_type']=='full_mug'){?>
 		     			<div>
-		     				<div class="mug_block parrot margin-right-25">
-		     					<div class="">
-                                                            <a href="<?php echo site_url('bar/list_message')?>">Messages</a>
-		     						<p class="mug_count">
-		     							<a href="<?php echo site_url('bar/list_message')?>"><?php echo $this->home_model->getmessagecount();?></a></p>
-                                                                <a href="<?php echo site_url('bar/postcard')?>">Post Cards</a>
-		     						<p class="mug_count">
-		     							<a href="<?php echo site_url('bar/postcard')?>"><?php echo $this->home_model->get_bar_postcard_count(@$getbar['bar_id']); ?></a></p>
-        
-                                                        </div>
+                                            <div class="mug_block parrot margin-right-25">
+                                                <div class="">
+                                                    <a href="<?php echo site_url('bar/list_message')?>">Messages</a>
+                                                        <p class="mug_count">
+                                                            <a href="<?php echo site_url('bar/list_message')?>"><?php echo $this->home_model->getmessagecount();?></a></p>
+                                                        <a href="<?php echo site_url('bar/postcard')?>">Post Cards</a>
+                                                        <p class="mug_count">
+                                                            <a href="<?php echo site_url('bar/postcard')?>"><?php echo $this->home_model->get_bar_postcard_count(@$getbar['bar_id']); ?></a></p>
                                                 </div>
+                                            </div>
                                         </div>
+                                        <?php } ?>
                                         <div id="list_show">	
                                             <div class="wrapper row6 padtb10 has-js">
                                                 <div class="container">
@@ -64,7 +64,7 @@
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
+                                </div>
                         </div>
         </div>
     </div>
