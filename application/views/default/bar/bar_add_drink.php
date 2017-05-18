e83c1c30794ada64bfa166d643d4191d030a86e6
Carleen <cpetrosi@lion.lmu.edu>
2017-05-18 11:18:07 -0700

    making add_drink consistent with dashboard menu

--- application/views/default/bar/bar_add_drink.php
+++ application/views/default/bar/bar_add_drink.php
@@ -10,17 +10,51 @@
 					   <?php echo $this->load->view(getThemeName().'/bar/cocktail_suggest');?>
 </div>	
 <div class="wrapper row6 padtb10 has-js">
-<div class="container">	
-<div id="list_show">	
-<div class="wrapper row6 padtb10 has-js">
-     	<div class="container">
-            <div class='pull-left' style="text-align: center;"><div class="result_search_text">What kind of drink you want to add?</div></div>
-     		<div class="margin-top-50 bg_brown" style="text-align: center;">
-                    <a class="btn btn-lg btn-primary marr_10"  href="<?php echo site_url('/bar/choose_cocktail');?>">Cocktail</a>
-                    <a class="btn btn-lg btn-primary marr_10" href="<?php echo site_url('/bar/choose_beer');?>">Beer</a>
-                    <a class="btn btn-lg btn-primary marr_10" href="<?php echo site_url('/bar/bar_liquor');?>">Liquor</a>
+    <div class="container">
+        <?php $theme_url = $urls= base_url().getThemeName();?>
+        <div class="wrapper row6 padtb10 has-js">
+            <div class="container">
+                <div class="margin-top-50 bg_brown">
+                        <?php echo $this->load->view(getThemeName().'/home/dashboard_menu'); ?>
+
+                        <div class="dashboard_detail">
+                            <div class="result_search">
+                                    <div class='pull-left'><div class="result_search_text">Bar Owner Dashboard</div></div>
+                                    <div class='pull-right'><div class="result_search_text"><a href="#userguide" data-toggle="modal" href="javascript://"  class="review mar_r15" >User Guide</a></div></div>
+                            <?php // if($getbar['bar_type']=='half_mug'){?>
+                                    <div class="pull-right marr_10">
+                                            <!-- <a href="#" class="review">Upgrade to Full Mug</a> -->
+                                            <a class="review" name="" href="<?php // echo site_url('home/registration_step3_upgrade/'.base64_encode($getbar['bar_id']).'/fullmug');?>">Upgrade to Full Mug</a>
+                                    </div>
+                            <?php // } ?>
+
+                            <?php // if(($getbar['bar_type']=='half_mug' || $getbar['bar_type']=='full_mug' && $getbar['is_managed']=='no') ){?>
+                                    <div class="pull-right marr_10">
+                                        <!-- <a href="#" class="review">Upgrade to Full Mug</a> -->
+                                        <a class="review" name="" href="<?php // echo site_url('home/registration_step3_upgrade/'.base64_encode($getbar['bar_id']).'/managed');?>">Upgrade to  Managed Account</a>
+                                    </div>
+                           <?php // } ?>
+                                <div class="clear"></div>
+                            </div>
+                            <div class="dashboard_subblock">
+                                <?php // if($getbar['bar_type']=='full_mug'){?>
+                                <?php // } ?>
+                                <div id="list_show">	
+                                    <div class="wrapper row6 padtb10 has-js">
+                                            <div class="container">
+                                                <div class='pull-left' style="text-align: center;"><div class="result_search_text">What kind of drink you want to add?</div></div>
+                                                    <div class="margin-top-50 bg_brown" style="text-align: center;">
+                                                        <a class="btn btn-lg btn-primary marr_10"  href="<?php echo site_url('/bar/choose_cocktail');?>">Cocktail</a>
+                                                        <a class="btn btn-lg btn-primary marr_10" href="<?php echo site_url('/bar/choose_beer');?>">Beer</a>
+                                                        <a class="btn btn-lg btn-primary marr_10" href="<?php echo site_url('/bar/bar_liquor');?>">Liquor</a>
+                                                    </div>
+                                            </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                 </div>
-        </div>
-</div>
             </div>
+        </div>
+    </div>
 </div>
