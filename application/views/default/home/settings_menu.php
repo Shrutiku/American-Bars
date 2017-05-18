3e8320c60df4c0868ae5e2f567e5686e61d51400
Carleen <cpetrosi@lion.lmu.edu>
2017-05-18 11:04:23 -0700

    testing dashboard

--- application/views/default/home/settings_menu.php
+++ application/views/default/home/settings_menu.php
@@ -14,206 +14,60 @@
 <div class="wrapper row6 padtb10 has-js">
     <div class="container">
         <div class="margin-top-50 bg_brown">
-            <?php echo $this->load->view(getThemeName().'/home/dashboard_menu'); ?>
+                <?php //echo $this->load->view(getThemeName().'/home/dashboard_menu'); ?>
 
-            <div class="dashboard_detail">
-                <div class="result_search">
-                        <div class='pull-left'><div class="result_search_text">Bar Owner Dashboard</div></div>
-                        <div class='pull-right'><div class="result_search_text"><a href="#userguide" data-toggle="modal" href="javascript://"  class="review mar_r15" >User Guide</a></div></div>
-                <?php if($getbar['bar_type']=='half_mug'){?>
-                        <div class="pull-right marr_10">
+                <div class="dashboard_detail">
+                    <div class="result_search">
+                            <div class='pull-left'><div class="result_search_text">Bar Owner Dashboard</div></div>
+                            <div class='pull-right'><div class="result_search_text"><a href="#userguide" data-toggle="modal" href="javascript://"  class="review mar_r15" >User Guide</a></div></div>
+                    <?php// if($getbar['bar_type']=='half_mug'){?>
+                            <div class="pull-right marr_10">
+                                    <!-- <a href="#" class="review">Upgrade to Full Mug</a> -->
+                                    <a class="review" name="" href="<?php echo site_url('home/registration_step3_upgrade/'.base64_encode($getbar['bar_id']).'/fullmug');?>">Upgrade to Full Mug</a>
+                            </div>
+                    <?php //} ?>
+
+                    <?php //if(($getbar['bar_type']=='half_mug' || $getbar['bar_type']=='full_mug' && $getbar['is_managed']=='no') ){?>
+                            <div class="pull-right marr_10">
                                 <!-- <a href="#" class="review">Upgrade to Full Mug</a> -->
-                                <a class="review" name="" href="<?php echo site_url('home/registration_step3_upgrade/'.base64_encode($getbar['bar_id']).'/fullmug');?>">Upgrade to Full Mug</a>
-                        </div>
-                <?php } ?>
-
-                <?php if(($getbar['bar_type']=='half_mug' || $getbar['bar_type']=='full_mug' && $getbar['is_managed']=='no') ){?>
-                        <div class="pull-right marr_10">
-                            <!-- <a href="#" class="review">Upgrade to Full Mug</a> -->
-                            <a class="review" name="" href="<?php echo site_url('home/registration_step3_upgrade/'.base64_encode($getbar['bar_id']).'/managed');?>">Upgrade to  Managed Account</a>
-                        </div>
-               <?php } ?>
-                    <div class="clear"></div>
-                </div>
-                <div class="dashboard_subblock">
-                    <?php if($getbar['bar_type']=='full_mug'){?>
-                    <div>
-                        <div class="mug_block parrot margin-right-25">
-                            <div class="">
-                                <a href="<?php echo site_url('bar/list_message')?>">Messages</a>
-                                    <p class="mug_count">
-                                        <a href="<?php echo site_url('bar/list_message')?>"><?php echo $this->home_model->getmessagecount();?></a></p>
-                                    <a href="<?php echo site_url('bar/postcard')?>">Post Cards</a>
-                                    <p class="mug_count">
-                                        <a href="<?php echo site_url('bar/postcard')?>"><?php echo $this->home_model->get_bar_postcard_count(@$getbar['bar_id']); ?></a></p>
+                                <a class="review" name="" href="<?php //echo site_url('home/registration_step3_upgrade/'.base64_encode($getbar['bar_id']).'/managed');?>">Upgrade to  Managed Account</a>
+                            </div>
+                   <?php //} ?>
+                        <div class="clear"></div>
+                    </div>
+                    <div class="dashboard_subblock">
+                        <?php //if($getbar['bar_type']=='full_mug'){?>
+                        <div>
+                            <div class="mug_block parrot margin-right-25">
+                                <div class="">
+                                    <a href="<?php echo site_url('bar/list_message')?>">Messages</a>
+                                        <p class="mug_count">
+                                            <a href="<?php echo site_url('bar/list_message')?>"><?php echo $this->home_model->getmessagecount();?></a></p>
+                                        <a href="<?php echo site_url('bar/postcard')?>">Post Cards</a>
+                                        <p class="mug_count">
+                                            <a href="<?php echo site_url('bar/postcard')?>"><?php echo $this->home_model->get_bar_postcard_count(@$getbar['bar_id']); ?></a></p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <?php } ?>
-                    <div id="list_show">	
-                        <div class="wrapper row6 padtb10 has-js">
-                            <div class="container">
-                                <div class='pull-left' style="text-align: center;"><div class="result_search_text">Select a settings function.</div></div>
-                                    <div class="margin-top-50 bg_brown" style="text-align: center;">
-                                        <a class="btn btn-lg btn-primary marr_10"  href="<?php echo site_url('/home/changepassword');?>">Change Password</a>
-                                        <a class="btn btn-lg btn-primary marr_10" href="<?php echo site_url('/home/domainmanagement');?>">Domain Management</a>
-                                        <a class="btn btn-lg btn-primary marr_10" href="<?php echo site_url('/home/barlistings');?>">Bar Listings</a>
-                                        <a class="btn btn-lg btn-primary marr_10" href="<?php echo site_url('/home/updatebanner');?>">Update Banner</a>
-                                        <a class="btn btn-lg btn-primary marr_10" href="<?php echo site_url('/home/updatecard');?>">Update Credit Card</a>
-                                    </div>
+                        <?php //} ?>
+                        <div id="list_show">	
+                            <div class="wrapper row6 padtb10 has-js">
+                                <div class="container">
+                                    <div class='pull-left' style="text-align: center;"><div class="result_search_text">Select a settings function.</div></div>
+                                        <div class="margin-top-50 bg_brown" style="text-align: center;">
+                                            <a class="btn btn-lg btn-primary marr_10"  href="<?php echo site_url('/home/changepassword');?>">Change Password</a>
+                                            <a class="btn btn-lg btn-primary marr_10" href="<?php echo site_url('/home/domainmanagement');?>">Domain Management</a>
+                                            <a class="btn btn-lg btn-primary marr_10" href="<?php echo site_url('/home/barlistings');?>">Bar Listings</a>
+                                            <a class="btn btn-lg btn-primary marr_10" href="<?php echo site_url('/home/updatebanner');?>">Update Banner</a>
+                                            <a class="btn btn-lg btn-primary marr_10" href="<?php echo site_url('/home/updatecard');?>">Update Credit Card</a>
+                                        </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
         </div>
     </div>
 </div>
 <script>
-    submitHandler: function(form){
-		$(form).ajaxSubmit({
-		type: "POST",
-		   		 dataType : 'json',
-				 beforeSubmit: function() 
-				 {
-		       		$('#dvLoading').fadeIn('slow');
-		    	 },
-		    	
-		    	uploadProgress: function ( event, position, total, percentComplete ) {	
-		        },
-		    
-		    	success : function ( json ) 
-		    	{
-		    		
-					if(json.status == "fail")
-					{
-						$("#cm-err-main1").show();
-						$("#cm-err-main1").html(json.comment_error);
-			    		$('#dvLoading').fadeOut('slow');
-			    		scrollToDivmore('dashboard_subblock');
-				  		// setTimeout(function () 
-						// {
-						      // $("#cm-err-main1").fadeOut('slow');
-						// }, 3000);
-					return false;
-					}
-			
-					else
-					{
-						if($("#cash_p").val()==0)
-						{
-							$("#cash_p").val(1)
-						}
-						else
-						{
-							$("#cash_p").val(0)
-						}
-						
-						if($("#master_p").val()==0)
-						{
-							$("#master_p").val(1)
-						}
-						else
-						{
-							$("#master_p").val(0)
-						}
-						
-						if($("#american_p").val()==0)
-						{
-							$("#american_p").val(1)
-						}
-						else
-						{
-							$("#american_p").val(0)
-						}
-						if($("#visa_p").val()==0)
-						{
-							$("#visa_p").val(1)
-						}
-						else
-						{
-							$("#visa_p").val(0)
-						}
-						if($("#paypal_p").val()==0)
-						{
-							$("#paypal_p").val(1)
-						}
-						else
-						{
-							$("#paypal_p").val(0)
-						}
-						
-						if($("#bitcoin_p").val()==0)
-						{
-							$("#bitcoin_p").val(1)
-						}
-						else
-						{
-							$("#bitcoin_p").val(0)
-						}
-						if($("#apple_p").val()==0)
-						{
-							$("#apple_p").val(1)
-						}
-						else
-						{
-							$("#apple_p").val(0)
-						}
-						//alert("sdsa");
-						$("#cm-err-main1").hide();
-						$("#cm-err-main1").html("");
-						
-						$.growlUI('Your profile updated successfully .');
-						//alert($('#serve_as').val());
-						if($('#serve_as_l').is(":checked")==true)
-					{
-						$("#ck_hide").addClass("gray_bg");
-						$("#lq_hide").removeClass("gray_bg");
-					}
-					else
-					{
-						$("#ck_hide").removeClass("gray_bg");
-						$("#lq_hide").addClass("gray_bg");
-					}
-					 	// $("#list_hide").slideDown();
-					 	 $("#list_hide_m").slideDown();
-					     $("#hd_del").slideDown();
-					     $("#hs_del").slideUp();
-					     $('#list_show').slideUp();
-					     $("#at_ajax").remove();
-					     getData();
-					     scrollToDivmore('dashboard_subblock');
-					}
-					$('#dvLoading').fadeOut('slow');
-		   		 }
-		    });
-		  }
-		})
-		
-		
-    });
-    
-    function getData()
-    {
-    	var id = '<?php echo @$getbar['bar_id']; ?>';
-    	$.ajax({
-	    type: "POST",
-            url: "<?php echo site_url('bar/getbardata')?>",
-            data : {id:id},
-            beforeSend : function() {
-                $('#dvLoading').fadeIn('slow');
-            },
-            complete : function() {
-                $('#dvLoading').fadeOut('slow');
-            },		
-            success: function(response) {
-                //alert('response');
-                //alert(response);
-                $('#list_hide').empty();
-                $("#list_hide").slideDown();
-                $('#list_hide').html(response);
-                initialize_map();
-            }
-	});
-    }
 </script>
