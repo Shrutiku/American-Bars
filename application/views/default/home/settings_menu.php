b06d3909c8c516c51f84dfef5217f19ef82113ed
Carleen <cpetrosi@lion.lmu.edu>
2017-05-18 11:07:54 -0700

    testing dashboard

--- application/views/default/home/settings_menu.php
+++ application/views/default/home/settings_menu.php
@@ -14,31 +14,31 @@
 <div class="wrapper row6 padtb10 has-js">
     <div class="container">
         <div class="margin-top-50 bg_brown">
-                <?php //echo $this->load->view(getThemeName().'/home/dashboard_menu'); ?>
+                <?php echo $this->load->view(getThemeName().'/home/dashboard_menu'); ?>
 
                 <div class="dashboard_detail">
                     <div class="result_search">
                             <div class='pull-left'><div class="result_search_text">Bar Owner Dashboard</div></div>
                             <div class='pull-right'><div class="result_search_text"><a href="#userguide" data-toggle="modal" href="javascript://"  class="review mar_r15" >User Guide</a></div></div>
-                    <?php// if($getbar['bar_type']=='half_mug'){?>
+                    <?php // if($getbar['bar_type']=='half_mug'){?>
                             <div class="pull-right marr_10">
                                     <!-- <a href="#" class="review">Upgrade to Full Mug</a> -->
-                                    <a class="review" name="" href="<?php //echo site_url('home/registration_step3_upgrade/'.base64_encode($getbar['bar_id']).'/fullmug');?>">Upgrade to Full Mug</a>
+                                    <a class="review" name="" href="<?php // echo site_url('home/registration_step3_upgrade/'.base64_encode($getbar['bar_id']).'/fullmug');?>">Upgrade to Full Mug</a>
                             </div>
-                    <?php //} ?>
+                    <?php // } ?>
 
-                    <?php //if(($getbar['bar_type']=='half_mug' || $getbar['bar_type']=='full_mug' && $getbar['is_managed']=='no') ){?>
+                    <?php // if(($getbar['bar_type']=='half_mug' || $getbar['bar_type']=='full_mug' && $getbar['is_managed']=='no') ){?>
                             <div class="pull-right marr_10">
                                 <!-- <a href="#" class="review">Upgrade to Full Mug</a> -->
-                                <a class="review" name="" href="<?php //echo site_url('home/registration_step3_upgrade/'.base64_encode($getbar['bar_id']).'/managed');?>">Upgrade to  Managed Account</a>
+                                <a class="review" name="" href="<?php // echo site_url('home/registration_step3_upgrade/'.base64_encode($getbar['bar_id']).'/managed');?>">Upgrade to  Managed Account</a>
                             </div>
-                   <?php //} ?>
+                   <?php // } ?>
                         <div class="clear"></div>
                     </div>
                     <div class="dashboard_subblock">
-                        <?php //if($getbar['bar_type']=='full_mug'){?>
+                        <?php // if($getbar['bar_type']=='full_mug'){?>
                         <div>
-                            <div class="mug_block parrot margin-right-25">
+<!--                            <div class="mug_block parrot margin-right-25">
                                 <div class="">
                                     <a href="<?php echo site_url('bar/list_message')?>">Messages</a>
                                         <p class="mug_count">
@@ -47,9 +47,9 @@
                                         <p class="mug_count">
                                             <a href="<?php echo site_url('bar/postcard')?>"><?php echo $this->home_model->get_bar_postcard_count(@$getbar['bar_id']); ?></a></p>
                                 </div>
-                            </div>
+                            </div>-->
                         </div>
-                        <?php //} ?>
+                        <?php // } ?>
                         <div id="list_show">	
                             <div class="wrapper row6 padtb10 has-js">
                                 <div class="container">
