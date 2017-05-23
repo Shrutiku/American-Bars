835cc096fe931b5208e58e868f1ab4b5170e99cd
Carleen <cpetrosi@lion.lmu.edu>
2017-05-23 11:10:37 -0700

    testing dashboard

--- application/views/default/home/dashboard_menu.php
+++ application/views/default/home/dashboard_menu.php
@@ -14,7 +14,7 @@
      							My Profile
      						</a>
      					</li>
-                                        <li class="<?php echo $this->uri->segment(2)=='dashboard' ? 'active':'';?> ">
+                                        <li class="<?php echo $this->uri->segment(2)=='updatebanner' ? 'active':'';?>">
      						<a href="<?php echo site_url('home/dashboard')?>">
      							<i class="strip cocktails"></i>
      							Drinks
