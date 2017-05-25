cc22d2ebfdadd4f067f366f07ca0905596a84577
Carleen <cpetrosi@lion.lmu.edu>
2017-05-25 14:47:17 -0700

    testing dashboard

--- application/views/default/bar/bar_special_hour.php
+++ application/views/default/bar/bar_special_hour.php
@@ -165,8 +165,8 @@ $('.sorted_table').sortable({
                                         <label class="control-label">Beers:</label>
                                     </div>
                                     <input type="hidden" name="bid<?php echo $i; ?>[]" id="bid_<?php echo $i; ?>_0" value="" />
-                                    <div class="col-sm-2">	
-                                        <input type="text" class="form-control tagsbeernew form-pad" id="beerid_<?php echo $i; ?>_0"  name="beerid[]" value="">
+                                    <div class="col-sm-2" style="padding-left: 15px;">	
+                                        <input type="text" class="form-control tagsbeernew" id="beerid_<?php echo $i; ?>_0"  name="beerid[]" value="">
                                     </div>
                                     <form action="">
                                         <input type="checkbox" name="day" value="Monday"> M
