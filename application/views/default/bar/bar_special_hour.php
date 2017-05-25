e5ba5425c61183bdba4a58819454109b45896d64
Carleen <cpetrosi@lion.lmu.edu>
2017-05-25 15:57:01 -0700

    testing dashboard

--- application/views/default/bar/bar_special_hour.php
+++ application/views/default/bar/bar_special_hour.php
@@ -158,7 +158,7 @@ $('.sorted_table').sortable({
                                                                                     <input type="checkbox" name="day" value="Wednesday"> W
                                                                                     <input type="checkbox" name ="day" value="Thursday"> Th
                                                                                     <input type="checkbox" name="day" value="Friday"> F
-                                                                                    <input type="checkbox" name="day" value="Saturday"> Sat
+                                                                                    <input type="checkbox" name="day" value="Saturday"> Sat <br>
                                                                                     <input type="checkbox" name="day" value="Sunday"> Sun
                                                                                 </form>
                                                                             </div>
