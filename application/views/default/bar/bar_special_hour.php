89b109696660e6959ea3c24f068e2cf5fdcc8118
Carleen <cpetrosi@lion.lmu.edu>
2017-05-25 15:31:04 -0700

    testing dashboard

--- application/views/default/bar/bar_special_hour.php
+++ application/views/default/bar/bar_special_hour.php
@@ -160,13 +160,13 @@ $('.sorted_table').sortable({
                                                                                 <input type="checkbox" name="day" value="Saturday"> Sat
                                                                                 <input type="checkbox" name="day" value="Sunday"> Sun
                                                                             </form>
-                                                                            <div class="col-sm-2 text-left">
+                                                                            <div class="col-sm-1 text-left">
                                                                                 <label class="control-label" style="font-size: 16px;">Time:</label>
                                                                             </div>
-                                                                            <div class="col-sm-2" style="width: 13%" >
+                                                                            <div class="col-sm-1" style="width: 13%" >
                                                                                 <input required type="text" value="<?php echo $im->hour_from; ?>"  class="timepicker-default form-control form-pad" name="hour_from[]" id="hour_from">
                                                                             </div>
-                                                                            <div class="col-sm-2 text-left"  style="width: 13%">	
+                                                                            <div class="col-sm-1 text-left"  style="width: 13%">	
                                                                                 <input required type="text" value="<?php echo $im->hour_to; ?>"  class="timepicker-default form-control form-pad" name="hour_to[]" id="hour_to">
                                                                             </div>	
                                                                             <div class="col-sm-2" style="width: 10%;">
