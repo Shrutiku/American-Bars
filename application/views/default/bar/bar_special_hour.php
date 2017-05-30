e0db283e2efaeb1a0946c015ad92060b5d1d3ed7
Shrutiku <shrutikulkarni1311@gmail.com>
2017-05-30 11:55:53 -0700

    div tst

--- application/views/default/bar/bar_special_hour.php
+++ application/views/default/bar/bar_special_hour.php
@@ -152,8 +152,7 @@ $('.sorted_table').sortable({
                                                                                 <label class="control-label" style="font-size: 16px;">Days:</label>
                                                                             </div>-->
                                                                             <div class="col-sm-3 padding-0">      
-                                                                            <div class="col-sm-2 ">     
-                                                                            </div>
+                                                                            
                                                                             <div class="col-sm-2">      
 
                                                                                 <label class="control-label" style="font-size: 16px;">Days:</label>
