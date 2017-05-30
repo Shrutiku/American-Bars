ebcd527dd90036a3365bd19b15d2e982bfb9ac57
Shrutiku <shrutikulkarni1311@gmail.com>
2017-05-30 16:06:30 -0700

    repeat rows

--- application/views/default/bar/bar_special_hour.php
+++ application/views/default/bar/bar_special_hour.php
@@ -2017,10 +2017,10 @@ function addrows(cnt){
 	   html +=                   			'<input type="text" class="form-control form-pad tagscocktail'+cnt+cntcocktail+'" id="cocktailid_'+cntcocktail+'"  name="cocktailid[]" value="">';
 	   html +=                   		'</div>';
            html+=  '<div class="col-sm-3 text-right">';
-                                                    html+=    '<label class="control-label">Days : <span class="aestrick"> * </span></label>';
+                                                    html+=    '<label class="control-label">Dayssss : <span class="aestrick"> * </span></label>';
                                                html+=     '</div>';
         html +=                       '<div class="col-sm-3" style="width: 10%; padding-left: 5px; padding-right: 5px;">';
-	   html +=    				 		'<label class="control-label " style="font-size: 16px;">Price : $</label>';
+	   html +=    				 		'<label class="control-label " style="font-size: 16px;">Priceee : $</label>';
 	   html +=    				 	'</div>';
 	   html +=                   		'<div class="col-sm-2" style="width: 10%;  padding-left: 5px; padding-right: 5px; margin-right:24px; ">';	
 	  html +=                    			'<input type="text" class="form-control form-pad timepicker-default" id="cocktailprice_'+cnt+cntcocktail+'" name="cocktailprice'+cnt+'[]" value="">';
