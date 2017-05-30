6426585d4b6859adae42dd73e888de03c915bca9
Anthony <anthony@americanbars.com>
2017-05-30 10:31:21 -0700

    fixing top of dashboard

--- application/views/default/home/dashboard.php
+++ application/views/default/home/dashboard.php
@@ -163,32 +163,40 @@ B.show();
 		     		<div class="dashboard_subblock">
 		     			<?php if($getbar['bar_type']=='full_mug'){?>
 		     			<div>
-		     				<div class="mug_block parrot margin-right-25">
+		     				<div class="mug_block parrot margin-right-30">
 		     					<div class="">
                                                             <a href="<?php echo site_url('bar/list_message')?>">Messages</a>
-		     						<p class="mug_count">
-		     							<a href="<?php echo site_url('bar/list_message')?>"><?php echo $this->home_model->getmessagecount();?></a></p>
-                                                                <a href="<?php echo site_url('bar/postcard')?>">Post Cards</a>
-		     						<p class="mug_count">
-		     							<a href="<?php echo site_url('bar/postcard')?>"><?php echo $this->home_model->get_bar_postcard_count(@$getbar['bar_id']); ?></a></p>
+                                                            <p class="mug_count">
+                                                                <a href="<?php echo site_url('bar/list_message')?>"><?php echo $this->home_model->getmessagecount();?></a>
+                                                            </p>
+                                                            
+<!--                                                            <a href="<?php // echo site_url('bar/comments')?>">Messages</a>
+                                                            <p class="mug_count">
+                                                                <a href="<?php // echo site_url('bar/comments')?>"><?php // echo $this->home_model->getcommentcount();?></a>
+                                                            </p>-->
+                                                            
+                                                            <a href="<?php echo site_url('bar/postcard')?>">Post Cards</a>
+                                                            <p class="mug_count">
+                                                                    <a href="<?php echo site_url('bar/postcard')?>"><?php echo $this->home_model->get_bar_postcard_count(@$getbar['bar_id']); ?></a>
+                                                            </p>
 		     					
 		     					</div>
 		     				</div>
 		     				
-		     				<div class="mug_block green margin-right-25">
+		     				<div class="mug_block green margin-right-30">
                                                     <a href="<?php echo site_url('bar/bar_beer')?>">Beers</a>
 		     					<p class="mug_count">
 		     						<a href="<?php echo site_url('bar/bar_beer')?>"><?php echo $this->home_model->countbeer(@$getbar['bar_id']);?></a></p>
-		     			<?php if($getbar['serve_as']=='cocktail'){?>		
+		     			<?php // if($getbar['serve_as']=='cocktail'){?>		
                                                         <a href="<?php echo site_url('bar/bar_cocktail')?>">Cocktails</a>
 		     					<p class="mug_count">
 		     						<a href="<?php echo site_url('bar/bar_cocktail')?>"><?php echo $this->home_model->countcocktail(@$getbar['bar_id']); ?></a></p>
-		     			<?php } ?>		
-		     			<?php if($getbar['serve_as']=='liquor'){?>
+		     			<?php // } ?>		
+		     			<?php // if($getbar['serve_as']=='liquor'){?>
                                                         <a href="<?php echo site_url('bar/bar_liquor')?>">Liquors</a>
 		     					<p class="mug_count">
 		     						<a href="<?php echo site_url('bar/bar_liquor')?>"><?php echo $this->home_model->countliquor(@$getbar['bar_id']); ?></a></p>
-		     			<?php } ?>		
+		     			<?php // } ?>		
 		     				</div>
 		     				<div class="mug_block brown ">
 		     					<div class="">
