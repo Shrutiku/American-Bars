3fb1cfc4bce484d0fa49e6ae2804bfcb142b2fb7
Carleen <cpetrosi@lion.lmu.edu>
2017-05-30 11:13:54 -0700

    testing dashboard

--- application/views/default/bar/bar_special_hour.php
+++ application/views/default/bar/bar_special_hour.php
@@ -297,11 +297,13 @@ $('.sorted_table').sortable({
                                                                                         <li><a value="Sunday">Sunday</a></li>
                                                                                     </ul>-->
                                                                                     <select class="form-control">
-                                                                                        <option value="one">One</option>
-                                                                                        <option value="two">Two</option>
-                                                                                        <option value="three">Three</option>
-                                                                                        <option value="four">Four</option>
-                                                                                        <option value="five">Five</option>
+                                                                                        <option value="monday">Monday</option>
+                                                                                        <option value="tuesday">Tuesday</option>
+                                                                                        <option value="wednesday">Wednesday</option>
+                                                                                        <option value="thursday">Thursday</option>
+                                                                                        <option value="friday">Friday</option>
+                                                                                        <option value="saturday">Saturday</option>
+                                                                                        <option value="sunday">Sunday</option>
                                                                                     </select>
                                                                                 </div>
                                                                                 <div class="col-sm-1">
