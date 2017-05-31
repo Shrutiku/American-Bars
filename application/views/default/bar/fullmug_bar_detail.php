a9e75cb962da73fbdf86685ce91fe6d687f621fb
Anthony <anthony@americanbars.com>
2017-05-31 13:33:37 -0700

    Fullmug Detail edits - payment type & Hours

--- application/views/default/bar/fullmug_bar_detail.php
+++ application/views/default/bar/fullmug_bar_detail.php
@@ -274,7 +274,7 @@
 				    	</div>
 
 				    	<div>
-                                            <div class="mar_top20 like-block wid50 mar_r15 text-right" style="width: 40%">
+                                            <div class="mar_top20 like-block wid50 mar_r15 text-right" style="width: 45%">
 				    		 	 <!-- <?php // $cnt_like = like_checker_bar($bar_detail['bar_id'],$this->session->userdata('user_id'));
 
 //								if($cnt_like==2 && get_authenticateUserID()!=''){
@@ -327,7 +327,7 @@
 			     				<?php } ?>
 		     				</div>
                                             <?php if($bar_detail['bar_category']){?>
-                                                <div class="socialicon-right mar_top20" style="width:26%">
+                                                <div class="socialicon-right mar_top20" style="width:30%">
                                                     <h4 class="bar_add mar_bot10">Bar Type :</h4>
                                                         <?php
                                                         $getin1 = explode(',',strip_tags($bar_detail['bar_category']));
@@ -346,7 +346,7 @@
 										 $bar_detail['american_p']==1){ ?>
 
 
-                                            <div class="mar_top20 wid50 like-block pull-right" style="width: 26%">
+                                            <div class="mar_top20 wid50 like-block pull-right" style="width: 27%">
 		     					<!--<div class="bar_add mar_bot10">Payment Type Accepted :</div>-->
 		     					<ul class="cashicon-list">
 		     						<?php if($bar_detail['cash_p']==1){?>
