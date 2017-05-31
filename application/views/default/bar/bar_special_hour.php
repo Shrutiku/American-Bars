e13ea245f79676d5f8a6ad2db03b6f0b65352dec
Carleen <cpetrosi@lion.lmu.edu>
2017-05-31 09:35:56 -0700

    testing dashboard

--- application/views/default/bar/bar_special_hour.php
+++ application/views/default/bar/bar_special_hour.php
@@ -286,7 +286,7 @@ $('.sorted_table').sortable({
 <!--                                                                                <div class="col-sm-1"> 
                                                                                     <label class="control-label" style="font-size: 16px">Time:</label>
                                                                                 </div>-->
-                                                                                <div class="col-sm-2 pull-left" style="width: 13%" >
+                                                                                <div class="col-sm-3 pull-left" style="width: 13%" >
                                                                                     <label class="control-label" style="font-size: 16px">Time:</label>
                                                                                     <input required type="text" value="<?php echo $im->hour_from; ?>"  class="timepicker-default form-control form-pad" name="hour_from[]" id="hour_from">
                                                                                 </div>
