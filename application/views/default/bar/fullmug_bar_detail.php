f236a0a954b0daeb158291cd4281a5965589b247
Anthony <anthony@americanbars.com>
2017-05-31 14:16:47 -0700

    Fullmug Detail edits - payment type & Hours

--- application/views/default/bar/fullmug_bar_detail.php
+++ application/views/default/bar/fullmug_bar_detail.php
@@ -327,7 +327,7 @@
 			     				<?php } ?>
 		     				</div>
                                             <?php if($bar_detail['bar_category']){?>
-                                                <div class="mar_top20 wid50 like-block" style="width:34%">
+                                                <div class="mar_top20 wid50 like-block" style="width:34%; height: 250px">
                                                     <h4 class="bar_add mar_bot10">Bar Type :</h4>
                                                         <?php
                                                         $getin1 = explode(',',strip_tags($bar_detail['bar_category']));
