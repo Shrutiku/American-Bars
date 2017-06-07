95dd40228f1a754c9e43d7409c2d5ae61bd9afef
Anthony <anthony@americanbars.com>
2017-06-07 14:12:04 -0700

    Bar Detail Edits

--- application/views/default/home/dashboard.php
+++ application/views/default/home/dashboard.php
@@ -391,7 +391,7 @@ B.show();
 	        				 		<label class="control-label">Street Address : <span class="aestrick"> * </span></label>
 	        				 	</div>
 	                       		<div class="input_box col-sm-7">
-                                            <textarea rows="1" placeholder="Address" name="address" id="address" class="form-control form-pad" value="<?php echo @$getalldata->address; ?>"></textarea>
+                                            <textarea rows="1" placeholder="Address" name="address" id="address" class="form-control form-pad" ><?php echo @$getalldata->address; ?></textarea>
 	                       		</div>
 	                       		<div class="clearfix"></div>
 	                       	</div>
