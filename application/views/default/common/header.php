b8ec63717d04b6e5f32ce19cfba2b04f5b85c35a
Carleen <cpetrosi@lion.lmu.edu>
2017-06-08 13:04:44 -0700

    testing home page

--- application/views/default/common/header.php
+++ application/views/default/common/header.php
@@ -107,7 +107,7 @@ $(function(){
             		<!-- <div class="row"> -->
                 		<div  class="search_box ">
                 			<form class=""  role="form" action="<?php echo site_url("bar/lists") ?>" method="post">
-                				<div class="pull-left mar_r10"><input type="text" name="bar_title_new" id="bar_title_new" value="<?php echo @base64_decode($bar_title_new); ?>" class="form-control bar_title_new" placeholder="By Name, City Or Zip"></div>
+                				<div class="pull-left mar_r10"><input type="text" name="bar_title_new" id="bar_title_new" value="<?php echo @base64_decode($bar_title_new); ?>" class="form-control bar_title_new" placeholder="Name, City Or Zip"></div>
                 				<input type="hidden" name="limit" id="limit" value="20" />
                     			<div class="pull-left"><button class="btn btn-lg btn-primary btn-block " type="submit"><span class="glyphicon glyphicon-search"></span></button></div>
                     		</form>
