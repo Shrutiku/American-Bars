7367b16dff2426091ef12aa71128da1afa568c57
Carleen <cpetrosi@lion.lmu.edu>
2017-06-08 12:54:44 -0700

    testing home page

--- application/views/default/common/header_home.php
+++ application/views/default/common/header_home.php
@@ -9,11 +9,11 @@
 $(function(){
 	$.placeholder.shim();
 });
-</script> 
+</script>
 
 <div class="modal fade" id="helpfindbar" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 	<?php //echo $this->load->view(getThemeName().'/bar/bar_suggest');?>
-</div>	
+</div>
 <div class="modal fade login_pop2" id="loginmodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" >
 					   <?php echo $this->load->view(getThemeName().'/home/login_ajax');?>
 					</div>
@@ -34,25 +34,25 @@ $(function(){
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-         
-          <a class="navbar-brand" href="https://www.facebook.com/AmericanBars" target="_blank"><i class="strip event_fb"> </i> <?php /*?><img src="<?php echo $theme_url; ?>/images/fb_icon.png"  /><?php */?></a>
+
+          <!-- <a class="navbar-brand" href="https://www.facebook.com/AmericanBars" target="_blank"><i class="strip event_fb"> </i> <?php /*?><img src="<?php echo $theme_url; ?>/images/fb_icon.png"  /><?php */?></a>
           <div class="fb-like" data-href="https://www.facebook.com/AmericanBars" data-layout="button" data-action="like" data-show-faces="true" data-share="true"></div>
           <a class="navbar-brand" href="https://twitter.com/American_Bars" target="_blank"><i class="strip event_twitter"> </i> <?php /*?><img src="<?php echo $theme_url; ?>/images/twitt_icon.png"/><?php */?></a>
           <a class="navbar-brand" href="#"><i class="strip linkdn"> </i> <?php /*?><img src="<?php echo $theme_url; ?>/images/linked_icon.png" /><?php */?></a>
-          <a class="navbar-brand" href="https://plus.google.com/+AmericanDiveBars" target="_blank"><i class="strip event_google"> </i> <?php /*?><img src="<?php echo $theme_url; ?>/images/google_icon.png"/><?php */?></a>
+          <a class="navbar-brand" href="https://plus.google.com/+AmericanDiveBars" target="_blank"><i class="strip event_google"> </i> <?php /*?><img src="<?php echo $theme_url; ?>/images/google_icon.png"/><?php */?></a> -->
           <!-- <div class="g-plusone" data-size="small" data-annotation="inline"  data-href="https://plus.google.com/+AmericanDiveBars"></div> -->
-          <a class="navbar-brand" href="https://in.pinterest.com/americandivebar/" target="_blank"><i class="strip event_pint"> </i> <?php /*?><img src="<?php echo $theme_url; ?>/images/p_icon.png"/><?php */?></a>
+          <!-- <a class="navbar-brand" href="https://in.pinterest.com/americandivebar/" target="_blank"><i class="strip event_pint"> </i> <?php /*?><img src="<?php echo $theme_url; ?>/images/p_icon.png"/><?php */?></a>
           <a class="navbar-brand" href="https://instagram.com/americanbars/" target="_blank"><i class="instagram-icon"> </i> <?php /*?><img src="<?php echo $theme_url; ?>/images/p_icon.png"/><?php */?></a>
-          <a class="navbar-brand" href="http://www.youtube.com/channel/UCWMZID-7tH4Ft1VjG6jWACg" target="_blank"><img src="https://americanbars.com/default/images/youtube.png" alt="Youtube Page" /></a>
+          <a class="navbar-brand" href="http://www.youtube.com/channel/UCWMZID-7tH4Ft1VjG6jWACg" target="_blank"><img src="https://americanbars.com/default/images/youtube.png" alt="Youtube Page" /></a> -->
         </div>
-        
+
         <div class="navbar-collapse collapse" id="b-menu-1">
-        	
+
           <ul class="nav navbar-nav">
-               <?php 
+               <?php
 		  if(check_user_authentication ()== ''){ ?>
               <form class=""  role="form" action="<?php echo site_url("bar/lists") ?>" method="post">
-                			<div class="pull-left"><input type="text" name="bar_title_new" id="bar_title_new" class="form-control bar_title_new" placeholder="By Name, City Or Zip"></div>
+                			<!-- <div class="pull-left"><input type="text" name="bar_title_new" id="bar_title_new" class="form-control bar_title_new" placeholder="By Name, City Or Zip"></div> -->
                     			<div class="pull-left"><button class="btn" style="background-color: #4CAF50;" type="submit">Claim Your Bar Now!</button></div>
               </form>
             <?php } ?>
@@ -61,15 +61,15 @@ $(function(){
             <?php }  elseif(check_user_authentication()) {?>
             <li><a href="<?php echo site_url('home/user_dashboard')?>" class="yellowlink">Profile</a></li>
             <?php } ?>
-            
+
           </ul>
           <ul class="nav navbar-nav navbar-right">
-          	  <?php 
+          	  <?php
 		  if(check_user_authentication ()!= ''){ $uinfo = get_user_info(get_authenticateUserID()); ?>
 		  <li class="user-name"><?php echo "Welcome,&nbsp&nbsp" . $uinfo->first_name; ?> </li>
-		  	
+
 		  	<?php } ?>
-           <?php 
+           <?php
 		  if(check_user_authentication ()!= ''){ ?>
           	<li><a href="<?php echo site_url('shopping/cart');?>" class="adjcarticon"><img src="<?php echo base_url().'default'?>/images/cart.png"/><span id="cartcount">
 			           				<?php
@@ -77,11 +77,11 @@ $(function(){
 										if($cartcount >0){ echo '<span class="itemcart">'.$cartcount.'</span>';}
 			           				?>
 			           				</span></a> </li>
-			           				
+
 			           				<?php } ?>
-            <?php 
+            <?php
 		  if(check_user_authentication ()!= ''){ ?>
-		  	
+
 	        <li><a href="<?php echo site_url("home/logout"); ?>" class="yellowlink">Logout</a></li>
 			<?php
 			}else{ ?>
@@ -95,9 +95,9 @@ $(function(){
           </ul>
         </div><!--/.nav-collapse -->
       </div>
-    </div>  
+    </div>
   </div>
-  <div class="wrapper row2"> 
+  <div class="wrapper row2">
     	<div class="header clearfix">
     		 <!-- <div class="container-fluid"> -->
     		 	<div class="container">
@@ -105,7 +105,7 @@ $(function(){
             			<a href="<?php echo site_url("home"); ?>"><img height="115"  width="489"  src="<?php echo $theme_url; ?>/images/americanbars.png" alt="American Bars" class="logo_img"/></a>
             		</div>
             		<!-- <div class="row"> -->
-                		<div  class="search_box">	
+                		<div  class="search_box">
                 			<form class=""  role="form" action="<?php echo site_url("bar/lists") ?>" method="post">
                 				<div class="pull-left mar_r10"><input type="text" name="bar_title_new" id="bar_title_new" value="<?php echo @base64_decode($bar_title_new); ?>" class="form-control bar_title_new" placeholder="By Name, City Or Zip"></div>
                 				<input type="hidden" name="limit" id="limit" value="20" />
@@ -114,17 +114,17 @@ $(function(){
                     		</form>
                     		<!-- <div class="clearfix"></div> -->
                             <!-- <a href="#helpfindbar" data-toggle="modal" class="btn btn-lg btn-primary text-center suggest-btn">Can't Find a Bar? Suggest One Now!</a> -->
-                             <a href="javascript://" onclick="openSug()" class="btn btn-lg btn-primary text-center suggest-btn">Can't Find a Bar? Suggest One Now!</a>
+                             <!-- <a href="javascript://" onclick="openSug()" class="btn btn-lg btn-primary text-center suggest-btn">Can't Find a Bar? Suggest One Now!</a> -->
                 		</div>
-                        
+
             		<!-- </div> -->
         		</div>
        		<!-- </div> -->
  		</div>
   </div>
   <div class="wrapper row3">
-  		
-    
+
+
     <div class="navbar navbar-default" role="navigation">
       <div class="container">
         <div class="navbar-header">
@@ -140,7 +140,7 @@ $(function(){
         </div>
         <div class="navbar-collapse collapse" id="b-menu-2">
           <ul class="nav navbar-nav">
-          	
+
           	<li><a href="<?php echo site_url("home"); ?>" class="<?php if($method=='home' && $function==''){?>active<?php } ?>">Home</a></li>
           	<li><a onclick="searchmodal()" href="javascript://"  class="<?php if($method=='bar' && ($function=='lists' || $function=='details')){?>active<?php } ?>">Find a Bar</a></li>
 			<!-- <li><a href="<?php echo site_url("beer"); ?>" class="<?php if($method=='beer' && ($function=='lists' || $function=='detail')){?>active<?php } ?>">beer directory </a></li> -->
@@ -154,7 +154,7 @@ $(function(){
                     <li><a href="<?php echo site_url('bar/gallery')?>">Photo Gallery</a></li>
           		</ul>
           	</li>
-          	
+
           		<li><a href="<?php echo site_url("trivia"); ?>" class="<?php if($method=='trivia' && ($function=='' || $function=='start')){?>active<?php } ?>">Bar Trivia Game</a></li>
           	<!-- <li><a href="javascript:void(0);">Media <span class="menu_slash">/</span class="menu_slash"></a></li> -->
           		<li class="dropdown">
@@ -166,22 +166,22 @@ $(function(){
                   <li><a href="<?php echo site_url('resource')?>">Resources</a></li>
                   <!-- <li><a href="<?php echo site_url('forum')?>">Groups</a></li> -->
                 </ul>
-          			
+
           	</li>
           	<!-- <li><a href="javascript:void(0);">Forum <span class="menu_slash">/</span class="menu_slash"></a></li> -->
           		<li class="dropdown">
           		 <a href="javascript://" class="dropdown-toggle" data-toggle="dropdown">Groups<b class="caret"></b></a>
           		  <ul class="sub-menu">
-          		  		<?php 
+          		  		<?php
           		  		$category = get_category();
           		  		if($category){
      								   foreach($category as $cat){?>
-     							 <li><a href="<?php echo site_url('forum/forums/'.$cat->forum_category_id);?>"><?php echo $cat->forum_category_name;?></a></li>	   	
-     					<?php } } ?>			   	
-                 
-                 
+     							 <li><a href="<?php echo site_url('forum/forums/'.$cat->forum_category_id);?>"><?php echo $cat->forum_category_name;?></a></li>
+     					<?php } } ?>
+
+
                 </ul>
-          			
+
           	</li>
           	<li><a class="<?php if($method=='home' && $function=='contact_us'){?>active<?php } ?>" href="<?php echo site_url('home/contact_us')?>">Contact Us</a></li>
           </ul>
@@ -191,12 +191,12 @@ $(function(){
           </ul> -->
         </div><!--/.nav-collapse -->
       </div>
-    </div> 
-     
+    </div>
+
     </div>
 
 <div class="modal fade login_pop2" id="searchmodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" >
-	  <?php echo $this->load->view(getThemeName().'/home/searchbox');?>	
+	  <?php echo $this->load->view(getThemeName().'/home/searchbox');?>
 </div>
 <div id="side-subscribe">
     <a id="side-link" title="Subscribe to American Bars" href="javascript:void(0);"><div class="side2">SUBSCRIBE NOW</div></a>
@@ -220,27 +220,27 @@ $(function(){
 		   },
 		    complete : function() {
 		   		$('#dvLoading').fadeOut('slow');
-		   },		
+		   },
 		   success: function(response) {
 		   	 $("#searchmodal").modal('hide');
 		    	$("#helpfindbar").html(function(){
 		   		$(this).html(response);
 		   		$('#helpfindbar').one('shown.bs.modal', function() {
-			           	 	
+
     						}).modal();
-    					
+
 		   	});
 		    //$('#mapmodal').one('show.bs.modal', function (e) {
 		    	//$('#myModalnew').one('shown.bs.modal', function (e) {
-		    	
-		     
+
+
 		  }
 	   });
     }
 	function searchmodal()
 	{
 		$("#searchmodal").modal('show');
-		 
+
 	}
 </script>
 <script>
