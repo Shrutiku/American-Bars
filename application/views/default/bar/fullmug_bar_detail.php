cb375af5fe70cb483c17f84cdcf60c239e79d971
Carleen <cpetrosi@lion.lmu.edu>
2017-06-13 13:48:22 -0700

    testing google reviews

--- application/views/default/bar/fullmug_bar_detail.php
+++ application/views/default/bar/fullmug_bar_detail.php
@@ -898,7 +898,7 @@ onKeyUp="limitText(event,this.form.desc_post_card,this.form.countdown,700 );" id
 		     					<div class="rating_box starrating<?php echo $comment->bar_rating; ?>"><a href="javascript"></a></div>
 		     					<div class="clearfix"></div>
 		     				</li>
-	     				<?php } } else {?> <br>
+	     				<?php } } else {?>
                                                 <h3>Reviews from Google</h3>
                                                 <!--<div class="gallery-default reviewdefault mar_top20" style="font-size:12px; text-align: left; max-height: 574px; vertical-align: top;">-->
                                                 <div id="google-reviews" style="font-size:14px; text-align: left; max-height: 574px;overflow-y:scroll;padding: 3px"><br><br></div>
