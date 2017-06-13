7497b75f5f794ed6d1844052d52545d2bc555ccd
Shrutiku <shrutikulkarni1311@gmail.com>
2017-06-13 13:19:41 -0700

    font

--- application/views/default/bar/fullmug_bar_detail.php
+++ application/views/default/bar/fullmug_bar_detail.php
@@ -898,7 +898,7 @@ onKeyUp="limitText(event,this.form.desc_post_card,this.form.countdown,700 );" id
 		     				</li>
 	     				<?php } } else {?>
                                                 <!--<div class="gallery-default reviewdefault mar_top20" style="font-size:12px; text-align: left; max-height: 574px; vertical-align: top;">-->
-                                                    <div id="google-reviews" style="font-size:12px; text-align: left; max-height: 574px;overflow-y:scroll;"></div>
+                                                    <div id="google-reviews" style="font-size:12px; text-align: left; max-height: 574px;overflow-y:scroll;border:solid;border-color:red"></div>
      				<!--</div>-->
 	     					<?php } ?>
 	     			</ul>
