ce72aa7a195f233b789965c781cce2c91e613018
Anthony <anthony@americanbars.com>
2017-06-13 13:44:34 -0700

    adding google reviews

--- default/js/google-places.js
+++ default/js/google-places.js
@@ -152,7 +152,7 @@
           for (var i = row_count; i >= 0; i--) {
             var stars = renderStars(reviews[i].rating);
             var date = convertTime(reviews[i].time);
-            html = html+"<div class='review-item'><div class='review-meta'><span class='review-author' style='font-size:16px; color:'#DEB863'>"+reviews[i].author_name+"</span><span class='review-sep'>, </span><span class='review-date pull-right' style='font-size:16px; color:'#DEB863'>"+date+"</span></div>"+stars+"<p class='review-text'>"+reviews[i].text+"</p></div>"
+            html = html+"<div class='review-item'><div class='review-meta'><span class='review-author' style='font-size:16px; color:'#DEB863'>"+reviews[i].author_name+"</span><span class='review-sep'> </span><span class='review-date pull-right' style='font-size:16px; color:'#DEB863'>"+date+"</span></div>"+stars+"<p class='review-text'>"+reviews[i].text+"</p></div><hr>"
           };
           $element.append(html);
         }
