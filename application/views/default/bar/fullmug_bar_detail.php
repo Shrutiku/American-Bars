74b79659b5590e901607bd509b758808b581ed1c
Anthony <anthony@americanbars.com>
2017-06-13 11:04:56 -0700

    adding google reviews

--- application/views/default/bar/fullmug_bar_detail.php
+++ application/views/default/bar/fullmug_bar_detail.php
@@ -871,6 +871,7 @@ onKeyUp="limitText(event,this.form.desc_post_card,this.form.countdown,700 );" id
  
 <link rel="stylesheet" type="text/css" href="<?php echo base_url().getThemeName(); ?>/application/bower_components/google-places/google-places.css" />
 <script type="text/javascript" src="<?php echo base_url().getThemeName(); ?>/application/bower_components/google-places/google-places.js"></script>              
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&libraries=places"></script>
 
      			<div class="review_mainblock padt10 new_review">
      				<h1 class="productbar_title">
