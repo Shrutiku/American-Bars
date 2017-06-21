df3e96066238e4901e38c62fcc283c83025fba49
Anthony <anthony@americanbars.com>
2017-06-20 19:44:25 -0700

    reverted api changes back to 0 stuff

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -38,21 +38,21 @@ class Api extends REST_Controller
         $this->response($data ,200);
     }
     
-//    function user_phone_check_post()
-//    {
-//        $phone_str = $this->input->post('phone');
-//        $phone_dash = filter_var($phone_str, FILTER_SANITIZE_NUMBER_INT);
-//        $phone = str_replace(array('+','-', '.'), '', $phone_dash);
-//        $num	=	$this->db->select('count(user_id) AS total')
-//                                         ->where("phone_no",$phone)
-//                                         ->where("user_type",'user')
-//                                         ->get("user_master")
-//                                         ->row()
-//                                         ->total; 
-//        
-//        $data['status'] = $num == 0 ? "unregistered" : "registered";   
-//        $this->response($data ,200);   	
-//    }
+    function user_phone_check_post()
+    {
+        $phone_str = $this->input->post('phone');
+        $phone_dash = filter_var($phone_str, FILTER_SANITIZE_NUMBER_INT);
+        $phone = str_replace(array('+','-', '.'), '', $phone_dash);
+        $num	=	$this->db->select('count(user_id) AS total')
+                                         ->where("phone_no",$phone)
+                                         ->where("user_type",'user')
+                                         ->get("user_master")
+                                         ->row()
+                                         ->total; 
+        
+        $data['status'] = (($num == 0) ? ("unregistered" : "registered"));   
+        $this->response($data ,200);   	
+    }
     
     function user_register_post()
 	{
