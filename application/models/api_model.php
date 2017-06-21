e547ff33e5441659fb82b23edf9db05094166bd2
Anthony Escobar <anthony@americanbars.com>
2017-06-21 16:40:18 -0700

    fixing login api

--- application/models/api_model.php
+++ application/models/api_model.php
@@ -99,6 +99,11 @@ class Api_model extends CI_Model
             $device_id = $this->input->post('device_id');
             $unique_code = uniqid().$device_id;
             $query = $this->db->get_where('user_master',array('phone_no'=>$phone,'password'=>md5($password),'user_type'=>'user'));
+            $name_update = array (
+                                            'first_name' => $this->input->post('first_name'),
+                                            'last_name' => $this->input->post('last_name')
+                                        );
+            $this->db->update('user_master',$name_update);
             
             if($query->num_rows() == 1)
             {
@@ -110,13 +115,6 @@ class Api_model extends CI_Model
                     $last_name= $user['last_name'];
                     $phone_no= $user['phone_no'];
                     $image= $user['profile_image'];
-                    
-                    $name_update = array (
-                                            'first_name' => $this->input->post('first_name'),
-                                            'last_name' => $this->input->post('last_name')
-                                        );
-                    $this->db->where('user_id',$user_id);
-                    $this->db->update('user_master',$name_update);
                 
                     if($status=='active')
                     {
