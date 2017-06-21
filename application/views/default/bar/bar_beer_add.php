551411be0ce435661003ffc750ba5d0ddb4cd0b8
American Bars <americanbars@Tobiass-iMac.local>
2017-06-21 15:58:22 -0700

    drinks

--- application/views/default/bar/bar_beer_add.php
+++ application/views/default/bar/bar_beer_add.php
@@ -59,20 +59,21 @@
 	                       			<button type="submit" class="btn btn-lg btn-primary marr_10" >Save</button> 
 	                       			<a  class="btn btn-lg btn-primary marr_10" href="<?php echo site_url('bar/add_drink');?>" >Cancel</a>
 	                       		</div>
+                                        <div class="col-sm-3 pull-right">
+                                            <label class="control-label">Can't find a beer?</label>
+                                            <!--<div class="pull-right">-->
+                                                <a href="#suggestmodal" onclick="blank()"  data-toggle="modal" class="btn btn-lg btn-primary">Suggest New Beer</a>
+                                            <!--</div>-->
+                                            <div class="modal fade login_pop2" id="suggestmodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" >
+                                                <?php echo $this->load->view(getThemeName().'/bar/beer_suggest');?>
+                                            </div>
+                                        </div>
 	                       		<div class="clearfix"></div>
 	                       	</div>
 	                       	<div class="clearfix"></div>
      		</div>
      			</form>
-                                <div class="col-sm-3">
-                                    <label class="control-label">Can't find a beer?</label>
-                                    <div class="pull-right">
-                                        <a href="#suggestmodal" onclick="blank()"  data-toggle="modal" class="btn btn-lg btn-primary">Suggest New Beer</a>
-                                    </div>
-                                    <div class="modal fade login_pop2" id="suggestmodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" >
-                                        <?php echo $this->load->view(getThemeName().'/bar/beer_suggest');?>
-                                    </div>
-                                </div>
+                                
      			</div>
      			
 		     	</div>
