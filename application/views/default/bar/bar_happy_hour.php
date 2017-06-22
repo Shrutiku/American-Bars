25a507858baa6e4cda1a25193f5529bd663c96b3
Carleen Petrosian <cpetrosi@lion.lmu.edu>
2017-06-22 14:38:17 -0700

    adding new happy hours

--- application/views/default/bar/bar_happy_hour.php
+++ application/views/default/bar/bar_happy_hour.php
@@ -116,7 +116,7 @@ $('.sorted_table').sortable({
                                                                     <input required type="text" value="<?php echo $im->hour_to; ?>"  class="timepicker-default form-control form-pad" name="hour_to[]" id="hour_to">
                                                                 </div>
                                                                 <div class="col-sm-1"></div>                   
-                                                                <a href="javascript://;" id="" onclick="addrows('<?php echo $i; ?>')" name="add_rowbeer" class="add_rowbeer btn btn-lg btn-primary search marr_10 pull-left"><span class="glyphicon glyphicon-plus "></span></a> 
+                                                                <a href="javascript://;" id="add_row" name="add_row" class="add_row btn btn-lg btn-primary search marr_10 pull-left"><span class="glyphicon glyphicon-plus "></span></a> 
                                                                 <div class="clearfix"></div>
                                                                      <!--<input type="password" class="form-control form-pad" id="email" placeholder="New Password" name="email" value="<?php echo @$email; ?>">-->         
                                                             </div>
