e535369afd635b1b9e08259b38a601ae71696fb9
Carleen Petrosian <cpetrosi@lion.lmu.edu>
2017-06-27 13:42:52 -0700

    adding new happy hours

--- application/views/default/bar/bar_happy_hour.php
+++ application/views/default/bar/bar_happy_hour.php
@@ -752,8 +752,8 @@ $('#add_row').click(function(){
                     html += '<label class="control-label" style="font-size: 16px;">Days:</label>';
                 html += '</div>';
                 html += '<div class="col-sm-2" style="padding-left: 0px; padding-right: 0px;"> '; 
-                html += '<input required name="day_from[]" id="day_from'+cnt+'" class="select_box">';
-                    html += '<select class="form-control">';
+                html += '<select required name="day_from[]" id="day_from'+cnt+'" class="select_box">';
+                    //html += '<select class="form-control">';
                         html += '<option value="">-- Select Day-- </option>';
                         html += '<option value="monday">Monday</option>';
                         html += '<option value="tuesday">Tuesday</option>';
@@ -768,8 +768,8 @@ $('#add_row').click(function(){
                     html += '<label class="control-label" style="font-size: 16px;">to</label>';
                 html += '</div>';
                 html += '<div class="col-sm-2" style="padding-left: 0px; padding-right: 0px;"> '; 
-                html += '<input required name="day_to[]" id="day_to'+cnt+'" class="select_box">';
-                    html += '<select class="form-control">';
+                html += '<select required name="day_to[]" id="day_to'+cnt+'" class="select_box">';
+//                    html += '<select class="form-control">';
                         html += '<option value="">-- Select Day-- </option>';
                         html += '<option value="monday">Monday</option>';
                         html += '<option value="tuesday">Tuesday</option>';
