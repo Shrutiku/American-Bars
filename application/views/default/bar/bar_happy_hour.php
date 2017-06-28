7a2d48e62deb7be631c1c178459a48d27d5b1dcf
Carleen Petrosian <cpetrosi@lion.lmu.edu>
2017-06-28 14:15:12 -0700

    adding new happy hours

--- application/views/default/bar/bar_happy_hour.php
+++ application/views/default/bar/bar_happy_hour.php
@@ -119,13 +119,12 @@ $('.sorted_table').sortable({
                                                                     <input required type="text" value="<?php echo $im->hour_to; ?>"  class="timepicker-default form-control form-pad" name="hour_to[]" id="hour_to" defaultTime="3:00 PM">
                                                                     <!--<span class="input-group-addon"><i class="glyphicon glyphicon-time"></i></span>-->
                                                                 </div>
-                                                                <div class="col-sm-1"></div>
-                                                                
-                                                                <?php if($i==0) {?>
-                                                                <a href="javascript://;" id="add_row" name="add_row" class="add_row btn btn-lg btn-primary search marr_10 pull-left"><span class="glyphicon glyphicon-plus "></span></a>
-                                                                <?php} else { ?>
-                                                                <a href="javascript://" class="btn btn-lg btn-primary search marr_10 pull-left" onclick="removeImageDive('<?php echo $i; ?>')"><span class="glyphicon glyphicon-minus"></span></a> 
-                                                                <?php } ?>
+                                                                <div class="col-sm-1">
+                                                                    <a href="javascript://;" id="add_row" name="add_row" class="add_row btn btn-lg btn-primary search marr_10 pull-left"><span class="glyphicon glyphicon-plus "></span></a>
+                                                                </div>
+                                                                <div class="col-sm-1">
+                                                                    <a href="javascript://" class="btn btn-lg btn-primary search marr_10 pull-left" onclick="removeImageDive('<?php echo $i; ?>')"><span class="glyphicon glyphicon-minus"></span></a> 
+                                                                </div>
                                                                 
                                                                 <div class="clearfix"></div>
                                                                      <!--<input type="password" class="form-control form-pad" id="email" placeholder="New Password" name="email" value="<?php echo @$email; ?>">-->         
