f875e9962896505518e2e64b5c770de96f8d933e
Anthony <anthony@americanbars.com>
2017-07-03 14:03:04 -0700

    fixing api login

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -107,8 +107,8 @@ class Api extends REST_Controller
                                              ->total;          
 
             if($num == 0) { 
-                $first_name = "n/a"; // $this->input->post('first_name'); //"NOTZERO";
-                $last_name = "n/a"; // $this->input->post('last_name');
+                $first_name = "not_given"; // $this->input->post('first_name'); //"NOTZERO";
+                $last_name = "not_given"; // $this->input->post('last_name');
             
                 $data = $this->api_model->user_phone_register_api($first_name,$last_name,$phone);
             }
@@ -124,8 +124,8 @@ class Api extends REST_Controller
                     $data['status']= 'success';
                 }
                 
-                if($user && $user['first_name'] && ($user['first_name'] === "n/a")) {
-                    $first_name = "direct input"; //$this->input->post('first_name'); //"test";
+                if($user && $user['first_name'] && ($user['first_name'] === "not given")) {
+                    $first_name = "direct input works"; //$this->input->post('first_name'); //"test";
                     $last_name = $this->input->post('last_name');
 
                     $this->api_model->user_phone_update_name_api($first_name,$last_name,$phone);
