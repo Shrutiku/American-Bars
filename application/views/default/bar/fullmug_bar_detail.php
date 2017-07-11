38b8b206df536d343ef0243ce3d94191236355d6
Anthony Escobar <anthony@americanbars.com>
2017-07-11 15:25:39 -0700

    adding everyday as option for hhours

--- application/views/default/bar/fullmug_bar_detail.php
+++ application/views/default/bar/fullmug_bar_detail.php
@@ -1163,7 +1163,7 @@ span.required {
 
 
 <!-- <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script> -->
-<script src="https://maps.googleapis.com/maps/api/js?&amp;libraries=places&key=AIzaSyCaR-yEveHScBem2Pe2belL47G7vhbUmGA"></script>
+<script src="https://maps.googleapis.com/maps/api/js?&amp;libraries=places"></script>
 
 <script>
 $(window).load(function(){
