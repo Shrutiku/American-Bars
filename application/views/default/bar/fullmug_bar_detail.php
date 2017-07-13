ec3d9b9f05af78ec5a10e3839013260e9568cd94
Anthony Escobar <anthony@americanbars.com>
2017-07-13 12:18:46 -0700

    street view

--- application/views/default/bar/fullmug_bar_detail.php
+++ application/views/default/bar/fullmug_bar_detail.php
@@ -498,9 +498,9 @@
                                                         </div>
                                                         <div class="pad20">
                                                             <?php // echo $bar_detail['lat']; ?><?php // echo $bar_detail['lang']; ?>
-                                                            <p>
+                                                            <p style="text-align:center;">
                                                                 <img src="//maps.googleapis.com/maps/api/streetview?size=600x300&location=<?php echo $bar_detail['lat'];?>,<?php echo $bar_detail['lang'];?>&pitch=0&key=AIzaSyA3USy3JEMB1_mCg_ud0UDKYrzEa8657Uo" border="1px">
-                                                                <img src="//maps.googleapis.com/maps/api/streetview?size=600x300&location=<?php echo $bar_detail['address'];?> <?php echo $bar_detail['city'];?>, <?php echo $bar_detail['state'];?> <?php echo $bar_detail['zip'];?>&pitch=0&key=AIzaSyA3USy3JEMB1_mCg_ud0UDKYrzEa8657Uo" border="1px">
+                                                                <img src="//maps.googleapis.com/maps/api/streetview?size=600x300&location=<?php echo $bar_detail['address'];?> <?php echo $bar_detail['city'];?>, <?php echo $bar_detail['state'];?> <?php echo $bar_detail['zipcode'];?>&pitch=0&key=AIzaSyA3USy3JEMB1_mCg_ud0UDKYrzEa8657Uo" border="1px">
                                                             </p>
 
                                                         </div>
