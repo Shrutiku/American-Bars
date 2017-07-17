cf9a2feae2b19260205a51545324c9ec72a46bec
Anthony <anthony@americanbars.com>
2017-07-17 13:55:05 -0700

    street view

--- application/models/api_model.php
+++ application/models/api_model.php
@@ -2593,7 +2593,8 @@ $minLon = $lang - rad2deg($rad/$R/cos(deg2rad($lat)));
 		return array();
 	}
 	function user_logout($user_id,$device_id,$unique_code){
-			$this->db->delete('device_master',array('user_id'=>$user_id,'device_name'=>$device_id,'unique_code'=>$unique_code));
+//			$this->db->delete('device_master',array('user_id'=>$user_id,'device_name'=>$device_id,'unique_code'=>$unique_code));
+                        $this->db->delete('device_master',array('user_id'=>$user_id,'device_name'=>$device_id));
 			return 1;
 	 }
 	
