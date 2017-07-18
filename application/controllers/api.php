b32701a88cf99fe46a43993b0b0b9dae307f6190
Anthony <anthony@americanbars.com>
2017-07-17 18:14:44 -0700

    api logout

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -1451,13 +1451,14 @@ class Api extends REST_Controller
 	}	
 	
 	 function user_logout_post(){
-		$user_id = $_POST['user_id'];
+//		$user_id = $_POST['user_id'];
 		$device_id = $_POST['device_id'];
 		$unique_code = $_POST['unique_code'];
-		$this->response(array('user_id'=>$user_id, 'device_id'=>$device_id,'unique_code'=>$unique_code),200);
-                $logout = $this->api_model->user_logout($user_id,$device_id,$unique_code);
-                $this->response($logout);
-		if($logout){
+//		$this->response(array('user_id'=>$user_id, 'device_id'=>$device_id,'unique_code'=>$unique_code),200);
+//                $logout = $this->api_model->user_logout($user_id,$device_id,$unique_code);
+                $logout = $this->api_model->user_logout_hotfix($device_id,$unique_code);
+//                $this->response($logout);
+		if($logout == 1){
 			$this->response(array('status'=>'success'),200);
 		} else {
 			$this->response(array('status'=>'fail'),200);
