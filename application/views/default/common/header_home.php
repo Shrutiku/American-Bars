3aab87f7de23ce213c72d78ea254720d5e0e60ea
Anthony <anthony@americanbars.com>
2017-07-19 15:28:55 -0700

    mobile page fixes

--- application/views/default/common/header_home.php
+++ application/views/default/common/header_home.php
@@ -104,9 +104,9 @@ $(function(){
     		 <!-- <div class="container-fluid"> -->
     		 	<div class="container">
         			<div class="logo" style="margin-top:5px; margin-bottom:5px;">
-            			<!--<a id="ablogo" href="<?php echo site_url("home"); ?>"><img src="<?php echo $theme_url; ?>/images/new_ab_logo_light.png" alt="American Bars" style="height:70px"/></a>-->
-            			<!--<a id="ablogo" href="<?php echo site_url("home"); ?>"><img src="<?php echo $theme_url; ?>/images/new_ab_logo_light_b.png" alt="American Bars" style="height:70px"/></a>-->
-            			<a id="ablogo" href="<?php echo site_url("home"); ?>"><img src="<?php echo $theme_url; ?>/images/new_ab_logo_light_gold.png" alt="American Bars"/></a>
+            			<!--<a id="ablogo" href="<?php echo site_url("home"); ?>"><img id="ablogo" src="<?php echo $theme_url; ?>/images/new_ab_logo_light.png" alt="American Bars"/></a>-->
+            			<!--<a id="ablogo" href="<?php echo site_url("home"); ?>"><img id="ablogo" src="<?php echo $theme_url; ?>/images/new_ab_logo_light_b.png" alt="American Bars"/></a>-->
+            			<a id="ablogo" href="<?php echo site_url("home"); ?>"><img id="ablogo" src="<?php echo $theme_url; ?>/images/new_ab_logo_light_gold.png" alt="American Bars"/></a>
             		</div>
             		<!-- <div class="row"> -->
                 		<div  class="search_box" style="margin-top:10px;">
