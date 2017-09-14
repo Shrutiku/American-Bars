2006a63defcc3a7d38d4efe20aaae11da1e10c29
Anthony <anthony@americanbars.com>
2017-09-14 11:15:00 -0700

    logout api

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -1458,7 +1458,7 @@ class Api extends REST_Controller
 //		logout = $this->api_model->user_logout($user_id,$device_id,$unique_code);
                 $logout = $this->api_model->user_logout_hotfix($device_id,$unique_code);
                 $this->response("logout var value: " + $logout);
-		if($logout == '1'){
+		if($logout == 1){
 			$this->response(array('status'=>'success'),200);
 		} else {
 			$this->response(array('status'=>'fail'),200);
